056b25929c24738b19aa81a6b7b6b7ba32ab1c5f
Ashutosh-JavaDev <139701057+Ashutosh-JavaDev@users.noreply.github.com>
2023-08-13 13:43:17 +0530

    add all

---
+++ string/palindrome.java
@@ -0,0 +1,26 @@
+//check  wheather the string is palindrome or not
+
+import java.util.*;
+public class palindrome{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str=sc.nextLine();
+		palindromecheck(str);
+	}
+	public static void palindromecheck(String str){
+		int rev=0;
+		String st="";
+		char ch[]=str.toCharArray();
+		for(int i=str.length()-1;i>=0;i--){
+			st=st+ch[i];
+		}
+		System.out.print(st+" ");
+		if(str.equals(st)){
+			System.out.println("\nString is palindrome");
+		}
+		else{
+			System.out.println("\nString is not palindrome");
+		}
+	}
+}
