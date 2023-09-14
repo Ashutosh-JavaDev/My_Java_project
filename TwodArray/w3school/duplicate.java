96ba0d02095c5074334855874133396cf572e8d5
Ashutosh Ayush <jbnashu297@gmail.com>
2023-09-14 20:52:46 +0530

    Duplicate

---
+++ TwodArray/w3school/duplicate.java
@@ -0,0 +1,32 @@
+package w3school;
+import java.util.*;
+
+public class duplicate {
+    // Function to count the total number of duplicate elements in an array
+    public static int countDuplicates(int arr[], int size) {
+        int count = 0;
+        System.out.print("The duplicate elements are : ");
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] == arr[j] && i != j) {
+                    count++;
+                    System.out.print(arr[i]+" ");
+                }
+            }
+        }
+        return count;
+    }
+
+   public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the array elements");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int totalDuplicates = countDuplicates(arr, size);
+        System.out.println("\nThe Total Duplicate Elements are: " + totalDuplicates);
+    }
+}
