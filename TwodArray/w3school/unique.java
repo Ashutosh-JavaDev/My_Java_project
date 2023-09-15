38f0cbb8093f361109153db0fd32483a04644658
Ashutosh Ayush <jbnashu297@gmail.com>
2023-09-15 17:11:03 +0530

    Array

--- TwodArray/w3school/unique.java
+++ TwodArray/w3school/unique.java
@@ -1,35 +1,32 @@
 package w3school;
 import java.util.*;
-
-public class unique {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array");
-        int size = sc.nextInt();
-        int arr[] = new int[size];
-        System.out.println("Enter the array elements");
-        
-        // Input array elements
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        
-        System.out.println("The unique elements are: ");
-        for (int i = 0; i < size; i++) {
-            boolean isUnique = true; // Assume the element is unique
-
-            // Check if the element is the same as any subsequent elements
-            for (int j = i + 1; j < size; j++) {
-                if (arr[i] == arr[j]) {
-                    isUnique = false; // Mark as not unique
+public class unique{
+    public static void unique(int arr[],int size){
+        System.out.print("Unique array:");
+        for(int i=0;i<size;i++){
+              boolean result=false;
+            for(int j=0;j<size;j++){
+                if(arr[i]==arr[j] && i!=j){
+                    result=true;
                     break;
                 }
             }
-
-            // If the element is unique, print it
-            if (isUnique) {
-                System.out.print(arr[i] + " ");
+            if(result!=true)
+            {
+                System.out.print(arr[i]+" ");
             }
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the siZe of the array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the array elements");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        unique(arr, size);
     }
 }
