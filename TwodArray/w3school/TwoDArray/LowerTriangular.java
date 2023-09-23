dc68422903674041a0b37e56f641c0e7cb747bd6
Ashutosh Ayush <jbnashu297@gmail.com>
2023-09-23 18:37:30 +0530

    Management

--- TwodArray/w3school/TwoDArray/LowerTriangular.java
+++ TwodArray/w3school/TwoDArray/LowerTriangular.java
@@ -1,11 +1,19 @@
+package w3school.TwoDArray;
 import java.util.*;
 public class  LowerTriangular{
-    public static void result(int arr[],int size){
+    public static void result(int arr[][],int size){
+        System.out.println("Original Matrix");
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
         System.out.println("Lower Triangular");
         for(int i=0;i<size;i++){
             for(int j=0;j<size;j++){
-                if(arr[i]>arr[j]){
-                    System.out.print(arr[j]+" ");
+                if(i>j){
+                    System.out.print(arr[i][j]+" ");
                 }
             }
         }
@@ -14,10 +22,12 @@ public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the size of the array");
     int size=sc.nextInt();
-    int arr[]=new int[size];
+    int arr[][]=new int[size][size];
     System.out.println("Enter the array elements");
     for(int i=0;i<size;i++){
-        arr[i]=sc.nextInt();
+        for(int j=0;j<size;j++){
+        arr[i][j]=sc.nextInt();
+        }
     }
 
     result(arr, size);
