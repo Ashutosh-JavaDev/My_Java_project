6e848171cb85f867a1ed9e008ebe593e6ec14ad7
Ashutosh Ayush <jbnashu297@gmail.com>
2023-10-19 09:18:50 +0530

    loop

---
+++ Loop/Tabble.java
@@ -0,0 +1,23 @@
+package Loop;
+
+import java.util.*;
+
+public class Tabble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Number of The Table");
+        int s = sc.nextInt();
+        System.out.println("Enter the  Starting Value For the Table");
+        int f = sc.nextInt();
+        System.out.println("Enter the  Last Number of  The Table");
+        int e = sc.nextInt();
+        System.out.println("Enter the Ending Value For the Table");
+        int l = sc.nextInt();
+        for (int i = s; i <= e; i++) {
+            for (int j = f; j <= l; j++) {
+                
+            }
+
+        }
+    }
+}
