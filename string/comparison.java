1119545a5a55622605fabfbf586cfd34399c3899
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-02 22:09:19 +0530

    File Modified

--- string/comparison.java
+++ string/comparison.java
@@ -2,13 +2,16 @@ import java.util.*;
 public class comparison {
     public static void compare(String str1,String str2){
         int result=str1.compareTo(str2);
-        if (result < 0) {
-            // str1 comes before str2
-        } else if (result > 0) {
-            // str1 comes after str2
-        } else {
-            // str1 and str2 are equal
-        }
+     if(result<0){
+        System.out.println("String One is Greater");
+     }
+     else if(result>0)
+     {
+        System.out.println("String Two IS greater");
+     }
+     else{
+        System.out.println("Both Are Equal");
+     }
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -16,6 +19,6 @@ public class comparison {
         String str1=sc.nextLine();
         System.out.println("Enter the Second String");
         String str2=sc.nextLine();
-
+        compare(str1, str2);
     }
 }
