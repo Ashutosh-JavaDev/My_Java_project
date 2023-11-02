ccd9bdf48a6783d66ac1828f9c8e1547d87083e5
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-02 22:04:44 +0530

    Java File Modified

---
+++ string/comparison.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class comparison {
+    public static void compare(String str1,String str2){
+        int result=str1.compareTo(str2);
+        if (result < 0) {
+            // str1 comes before str2
+        } else if (result > 0) {
+            // str1 comes after str2
+        } else {
+            // str1 and str2 are equal
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Fi")
+    }
+}
