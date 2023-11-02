3e98ea5b4cf2b5f270dd60c9f9d57a1de7ed7699
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-02 22:13:31 +0530

    File Modified

--- string/comparison.java
+++ string/comparison.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class comparison {
     public static void compare(String str1,String str2){
         int result=str1.compareTo(str2);
-     if(result<0){
+     if(result>0){
         System.out.println("String One is Greater");
      }
-     else if(result>0)
+     else if(result<0)
      {
         System.out.println("String Two IS greater");
      }
