1ffcdb14bc421b57271ad78695ab2bea175ba4ea
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-03 17:16:33 +0530

    String Added

---
+++ string/WithoutbuiltinLength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class WithoutbuiltinLength {
+    public static  int lambai(String str){
+        int c=0;
+        for(int i=0;i<str.length();i++){
+            c++;
+        }
+        return c;
+    }
+    public  static void main(String[]args){
+        
+}
