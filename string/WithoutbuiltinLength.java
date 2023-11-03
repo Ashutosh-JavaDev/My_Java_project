27c78382e832c704e188780e714ac0ebec3429cb
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-03 17:17:43 +0530

    String Added

--- string/WithoutbuiltinLength.java
+++ string/WithoutbuiltinLength.java
@@ -10,7 +10,7 @@ public class WithoutbuiltinLength {
     public  static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter thee  Senetence");
-        String str=sc.nextLIne();
+        String str=sc.nextLine();
     }
         
 }
