73d54a6a2bc7cca3f26a9d20becee405d97e67a2
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-04 12:58:12 +0530

    Java File MOdified

--- string/Reverse.java
+++ string/Reverse.java
@@ -1,8 +1,18 @@
 import java.util.*;
+
 public class Reverse {
-    public static void reverse(String str){
-        for(int i=str.length();i>=0;i--){
-            System.out.print(i+" ");
+    public static void reverse(String str) {
+        StringBuilder reversedString = new StringBuilder(str);
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            System.out.print(ch);
         }
-    }    
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence");
+        String str = sc.nextLine();
+        reverse(str);
+    }
 }
