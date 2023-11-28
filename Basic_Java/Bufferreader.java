82b7b3f6a4f219480248e4392d8a64b68509dc40
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 09:33:23 +0530

    Java Program

--- Basic_Java/Bufferreader.java
+++ Basic_Java/Bufferreader.java
@@ -1,11 +1,21 @@
 package Basic_Java;
+
 import java.io.*;
+
 public class Bufferreader {
-    public static void main(String[]args)throws IOException{
-        InputStreamReader read=new InputStreamReader(System.in);
-        BufferedReader sc=new BufferedReader(read);
+    public static void main(String[] args) throws IOException {
+        InputStreamReader read = new InputStreamReader(System.in);
+        BufferedReader sc = new BufferedReader(read);
         System.out.println("Enter Roll Number");
-        int roll=Integer.parseInt(sc.readLine());
+        int roll = Integer.parseInt(sc.readLine());
+        System.out.println("Enter Name");
+        String Name = sc.readLine();
+        System.out.println("Enter Feee");
+        Double Fee = Double.parseDouble(sc.readLine());
+        System.out.println("Enter the Grade");
+        char grade = (char) sc.read();
+        System.out.println("Enter Percentage");
+        Float per = Float.parseFloat(sc.readLine());
 
     }
 }
