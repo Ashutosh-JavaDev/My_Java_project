a4c17a12ebb1447a955796140b5abcdfbc546910
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 13:05:53 +0530

    Program

--- Basic_Java/BufferInput/CheckMajorityElement.java
+++ Basic_Java/BufferInput/CheckMajorityElement.java
@@ -4,15 +4,17 @@ import java.io.*;
 public class CheckMajorityElement {
     public static void majority(int arr[],int size){
         int Duplicate=0;
+        int check=0;
         for(int i=0;i<size;i++){
           for(int j=i+1;j<size;j++){
             if(arr[i]==arr[j]&&i!=j){
                 Duplicate++;
+                check=Duplicate;
             }
           }
         }
         if(Duplicate>(size%2)){
-            System.out.println("One Number is More than Size");
+            System.out.println("One Number is More than Size and the total Outcome is "+check);
         }
         else{
             System.out.println("No Number is  More than Size");
@@ -24,6 +26,10 @@ public class CheckMajorityElement {
         System.out.println("Enter the size of the Array");
         int size=Integer.parseInt(sc.readLine());
         int arr[]=new int[size];
-        
+        System.out.println("Enter the Array Elements");
+        for(int i=0;i<size;i++){
+            arr[i]=Integer.parseInt(sc.readLine());
+        }   
+        majority(arr, size);
     }
 }
