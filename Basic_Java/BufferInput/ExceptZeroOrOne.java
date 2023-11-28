9659eac0d0d48deee538179af19582edfeb8a97d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 14:11:01 +0530

    Program

--- Basic_Java/BufferInput/ExceptZeroOrOne.java
+++ Basic_Java/BufferInput/ExceptZeroOrOne.java
@@ -1,30 +1,33 @@
 // Write a Java program to check if an array of integers is without 0 and -1.
 package Basic_Java.BufferInput;
+
 import java.io.*;
+
 public class ExceptZeroOrOne {
-    public static boolean except(int arr[],int size){
-        boolean result=false;
+    public static boolean except(int arr[], int size) {
         for(int i=0;i<size;i++){
             if(arr[i]==-1||arr[i]==0){
-                result=true;
-            }
-            else{
-                System.out.println("Array Do Not -1 and 0");
+                return true;
             }
         }
-        return result;
+        return false;
     }
-    public static void main(String[]args)throws IOException{
-        InputStreamReader read=new InputStreamReader(System.in);
-        BufferedReader sc=new BufferedReader(read);
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader read = new InputStreamReader(System.in);
+        BufferedReader sc = new BufferedReader(read);
         System.out.println("Enter the size of the array");
-        int size=Integer.parseInt(sc.readLine());
-        int arr[]=new int[size];
+        int size = Integer.parseInt(sc.readLine());
+        int arr[] = new int[size];
         System.out.println("Enter the Array Elements");
-        for(int i=0;i<size;i++){
-            arr[i]=Integer.parseInt(sc.readLine());
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(sc.readLine());
         }
-        Boolean Output=except(arr, size);
+      if(except(arr, size)){
         System.out.println("Array Contains -1 or 0");
+      }
+      else{
+        System.out.println("Array Not Contains -1 or 0");
+      }
     }
 }
