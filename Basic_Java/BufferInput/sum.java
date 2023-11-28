ebaeab54ea57ddfba4a0fc4425398d2a9fbd47a7
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 09:49:33 +0530

    Program

---
+++ Basic_Java/BufferInput/sum.java
@@ -0,0 +1,9 @@
+package Basic_Java.BufferInput;
+import java.io.*;
+public class sum {
+    public static void main(String[]args)throws IOException{
+        InputStreamReader read=new InputStreamReader(System.in);
+        BufferedReader sc=new BufferedReader(read);
+        
+    }
+}
