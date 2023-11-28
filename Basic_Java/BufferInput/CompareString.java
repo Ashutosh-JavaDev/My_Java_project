a962934ff7343c03456aaebaff955c1ac9dd9e1e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 10:52:32 +0530

    Java Program

--- Basic_Java/BufferInput/CompareString.java
+++ Basic_Java/BufferInput/CompareString.java
@@ -2,6 +2,19 @@
 package Basic_Java.BufferInput;
 import java.io.*;
 public class CompareString {
+    public static void compare(String str1,String str2){
+        int Result=str1.compareTo(str2);
+        if(Result>0){
+        System.out.println("String 1 is Greater");
+        }
+
+        else if(Result<0){
+            System.out.println("String 2 is Greater");
+        }
+        else{
+            System.out.println("Both Strings are Equal");
+        }
+    }
     public static void main(String[]args)throws IOException{
         InputStreamReader read=new InputStreamReader(System.in);
         BufferedReader sc=new BufferedReader(read);
@@ -9,7 +22,6 @@ public class CompareString {
         String Line1=sc.readLine();
         System.out.println("Enter the Second String");
         String Line2=sc.readLine();
-        int Result=Line1.compareTo(Line2);
-        System.out.println("Greater : "+Result);
+       compare(Line1, Line2);
     }
 }
