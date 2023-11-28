43d11a44aa80d1eb30ec68f20dfd1759c4db2caf
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 12:18:22 +0530

    Program

---
+++ Basic_Java/CombinedSum.java
@@ -0,0 +1,40 @@
+package Basic_Java;
+import java.util.*;
+public class CombinedSum {
+    public static void Sum(int Number,int size){
+        int output=0;
+        int c=0;
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                for(int k=j+1;k<size;k++){
+                    for(int l=k+1;l<size;l++){
+                        output=arr[i]+arr[j]+arr[k]+arr[l];
+                        if(output==Number){
+                            System.out.println("NUmber found");
+                        }
+                        else{
+                            c++;
+                        }
+                    }
+                }
+            }
+        }
+        if(c>0){
+            System.out.println("NO Commbintion Found");
+        }
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Array Elements");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the Numbber want to  check the  combination");
+        int Number=sc.nextInt();
+        Sum(size,Number);
+    }
+}
--- TwodArray/four_elements_sum.java
+++ TwodArray/four_elements_sum.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class four_elements_sum{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("WEnter the size of the array");
+		System.out.println("Enter the size of the array");
 		int size=sc.nextInt();
 		int arr[]=new int[size];
 		System.out.println("Enter the elements of array");
