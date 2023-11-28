f045090fcb23a2e0070ab8cf361dce1c48dc215f
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-28 09:54:32 +0530

    Java Program

--- Basic_Java/BufferInput/sum.java
+++ Basic_Java/BufferInput/sum.java
@@ -4,6 +4,11 @@ public class sum {
     public static void main(String[]args)throws IOException{
         InputStreamReader read=new InputStreamReader(System.in);
         BufferedReader sc=new BufferedReader(read);
-        
+        System.out.println("Enter First Number");
+        int Sum1=Integer.parseInt(sc.readLine());
+        System.out.println("Enter Second Number");
+        int Sum2=Integer.parseInt(sc.readLine());
+        int Result=Sum1+Sum2;
+        System.out.println("Result  : "+Result);
     }
 }
