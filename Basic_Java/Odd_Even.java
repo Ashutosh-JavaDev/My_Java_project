98106380fd6a046fa274efe66505c7af2fe816a4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-29 21:14:32 +0530

    Java Program

---
+++ Basic_Java/Odd_Even.java
@@ -0,0 +1,18 @@
+package Basic_Java;
+import java.util.*;
+public class Odd_Even {
+    public static void Result(int Number){
+        if(Number%2==0){
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is  Odd");
+        }
+    }
+    public static  void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int Number=sc.nextInt();
+        Result(Number);
+    }
+}
