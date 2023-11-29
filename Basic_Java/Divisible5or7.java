512e4dd1f4283b5d500d8f829fbd46e70240ccb7
Ashutosh Ayush <jbnashu297@gmail.com>
2023-11-29 21:41:06 +0530

    Java Program

---
+++ Basic_Java/Divisible5or7.java
@@ -0,0 +1,20 @@
+package Basic_Java;
+import java.util.*;
+public class Divisible5or7 {
+    public static boolean result(int number) {
+        boolean output = false;
+        if ((number % 5 == 0) && (number % 7 == 0)) {
+            output = true;
+        } else {
+            System.out.println("Number is not Divisible by 5 and 7 both");
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number wants to check");
+        int number=sc.nextInt();
+        result(number);
+    }
+}
