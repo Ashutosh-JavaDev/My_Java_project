972e5b01eba2860a182eafb2bde3877f90ea9ac4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-03 23:36:02 +0530

    Javascript Program

--- Practice_java/Concatinate.java
+++ Practice_java/Concatinate.java
@@ -2,6 +2,7 @@ package Practice_java;
 import java.util.*;
 public class Concatinate {
     public static void result(String str1,String str2){
-        
+        String output=str1+" "+str2;
+        System.out.println(output);
     }
 }
