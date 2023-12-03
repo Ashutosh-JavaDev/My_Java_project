c4017d3e2ddf89fcfa85449a1be9a2a729bf6ad2
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-03 23:38:37 +0530

    Javascript Program

--- Practice_java/Concatinate.java
+++ Practice_java/Concatinate.java
@@ -1,8 +1,19 @@
 package Practice_java;
+
 import java.util.*;
+
 public class Concatinate {
-    public static void result(String str1,String str2){
-        String output=str1+" "+str2;
+    public static void result(String str1, String str2) {
+        String output = str1 + " " + str2;
         System.out.println(output);
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two String");
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        result(str1, str2);
+    }
+
 }
