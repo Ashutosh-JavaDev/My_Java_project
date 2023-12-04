828fe4061c88eece9610c3f5d6a10d635b88a79d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-04 15:41:43 +0530

    Java Program

--- Practice_java/Password.java
+++ Practice_java/Password.java
@@ -1,26 +1,26 @@
-//WAP tp create a passwqord page for the user
 package Practice_java;
 
 import java.util.*;
 
 public class Password {
     public static boolean ComparePassword(String str1) {
-        boolean result=true;
-        if (str1.length() >= 8) {
-            char firstchar = str1.charAt(0);
-            if (firstchar >= 'A' && firstchar <= 'Z') {
-                System.out.println("Password is Valid");
-            } else {
-                System.out.println("Password must start with Alphabet letter");
-            }
+        boolean result = true;
+        if (str1.length() <= 8) {
+            System.out.println("Password must contain at least 8 characters");
+            result = false;
         } else {
-            System.out.println("Password must contains more than that 8 letter");
+            char firstchar = str1.charAt(0);
+            if (!(firstchar >= 'A' && firstchar <= 'Z')) {
+                System.out.println("Password must start with an alphabet letter");
+                result = false;
+            } else {
+                System.out.println("Password is Valid");
+            }
         }
         return result;
     }
 
     public static void compare(String str1, String str2) {
-        ComparePassword(str1);
         if (str1.equals(str2)) {
             System.out.println("Password is Set");
         } else {
@@ -32,10 +32,10 @@ public class Password {
         Scanner sc = new Scanner(System.in);
         System.out.println("New Password");
         String str1 = sc.nextLine();
-        ComparePassword(str1);
-        if(ComparePassword(str1)==true){
+        // ComparePassword(str1);
+        if (ComparePassword(str1)) {
             System.out.println("Confirm Password");
-            String str2=sc.nextLine();
+            String str2 = sc.nextLine();
             compare(str1, str2);
         }
     }
