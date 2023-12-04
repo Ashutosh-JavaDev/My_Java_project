b10acb7055dbcc6f2a1aab817d54b2c0420da100
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-04 10:24:09 +0530

    Javascript Program

--- Practice_java/Concatinate.java
+++ Practice_java/Concatinate.java
@@ -3,17 +3,26 @@ package Practice_java;
 import java.util.*;
 
 public class Concatinate {
-    public static void result(String str1, String str2) {
-        String output = str1 + " " + str2;
-        System.out.println(output);
+    public static void result(String arr[], int size) {
+        String newString = "";
+        for (int i = 0; i < size; i++) {
+            newString = newString + " " + arr[i];
+        }
+        System.out.println(newString);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Two String");
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
-        result(str1, str2);
+        System.out.println("Enter the approx value of line you will right");
+        int size = sc.nextInt();
+        // Consume the newline character left in the buffer
+        sc.nextLine();
+        String arr[] = new String[size];
+        System.out.println("Enter the Sentences");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextLine();
+        }
+        result(arr, size);
     }
 
 }
