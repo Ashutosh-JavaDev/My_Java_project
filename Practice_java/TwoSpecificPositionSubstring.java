fb34cf44721c36752795fb2b720efaaf7e9c35b4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-05 11:20:14 +0530

    Java Program

--- Practice_java/TwoSpecificPositionSubstring.java
+++ Practice_java/TwoSpecificPositionSubstring.java
@@ -10,8 +10,13 @@ public class TwoSpecificPositionSubstring {
     }
     public  static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the  Sentence");
-        
-
+        System.out.println("Enter the Sentence");
+        String str1=sc.nextLine();
+        System.out.println(("Enter the Value of starting Index"));
+        int startingIndex=sc.nextInt();
+        System.out.println("ENter the Value of Ending Index");
+        int endingIndex=sc.nextInt();
+        String Output=result(str1, startingIndex, endingIndex);
+        System.out.println(Output);
     }
 }
