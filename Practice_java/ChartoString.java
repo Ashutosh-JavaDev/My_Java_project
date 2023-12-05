967f4ff096f25d44a04500737e867d6388ac8900
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-05 11:37:42 +0530

    Java Program

--- Practice_java/ChartoString.java
+++ Practice_java/ChartoString.java
@@ -2,8 +2,14 @@
 package Practice_java;
 import java.util.*;
 public class ChartoString {
-    public static String result(char chr[]){
+    public static String result(char chr[],int size){
         String answer="";
+        for(int i=0;i<size;i++){
+            answer=answer+chr[i];
+        }
         return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
     }
 }
