c31d74449248a9189d8d85e2c4f5d40b65e11d8d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-05 11:16:27 +0530

    Java Program

---
+++ Practice_java/TwoSpecificPositionSubstring.java
@@ -0,0 +1,17 @@
+// Write a Java program to get a substring of a given string at two specified positions.
+package Practice_java;
+
+import java.util.*;
+
+public class TwoSpecificPositionSubstring {
+    public static String result(String str1, int str2, int str3) {
+        String str4 = str1.substring(str2, str3);
+        return str4;
+    }
+    public  static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  Sentence");
+        
+
+    }
+}
