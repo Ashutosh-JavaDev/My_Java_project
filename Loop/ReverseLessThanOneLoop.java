ef290e454f40343c64cf5937a28444b872f731ff
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-12 10:27:45 +0530

    Java Program

---
+++ Loop/ReverseLessThanOneLoop.java
@@ -0,0 +1,11 @@
+package Loop;
+public class ReverseLessThanOneLoop {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
