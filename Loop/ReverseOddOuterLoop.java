6d8865449016334e2182a6285179d7d3de5cffad
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-12 10:17:56 +0530

    Java Program

---
+++ Loop/ReverseOddOuterLoop.java
@@ -0,0 +1,11 @@
+package Loop;
+public class ReverseOddOuterLoop {
+    public static void main(String[] args) {
+        for(int i=9;i>=1;i-=2){
+            for(int j=1;j<=i;j+=2){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
