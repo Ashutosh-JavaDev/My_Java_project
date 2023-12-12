81b09fed28915107194737d2fd12422c7b900a8e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-12 09:54:55 +0530

    Java program

--- Loop/lessThanOneLoop.java
+++ Loop/lessThanOneLoop.java
@@ -1,5 +1,5 @@
 package Loop;
-public class EqualInnerOuterLoop {
+public class lessThanOneLoop {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
             for(int j=i;j<i*2;j++){
