ed34d4169d64bacc8a7e0a91e47c953917b3873f
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-20 22:52:59 +0530

    Project

--- recursion/BinarySearch.java
+++ recursion/BinarySearch.java
@@ -1,30 +1,34 @@
 import java.util.*;
+
 public class BinarySearch {
-    public static int search(int arr[],int target,int start,int end){
-        if(start>end){
+    public static int search(int arr[], int target, int start, int end) {
+        if (start > end) {
             return -1;
         }
-        int mid=start+(end-start)/2;
-        
-       if(target==arr[mid]){
+        int mid = start + (end - start) / 2;
+
+        if (target == arr[mid]) {
             return arr[mid];
         }
-        if(target>arr[mid]){
-            return search(arr, target, start+1, end);
+        if (target > arr[mid]) {
+            return search(arr, target, start + 1, end);
+        } else {
+            return search(arr, target, start, end - 1);
         }
-        else{
-            return search(arr, target, start, end-1);
-        }
-    }    
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of the Array")
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        int start = 0, end = size - 1;
         System.out.println("Enter the value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Enter the Value want to Search")
+        System.out.println("Enter the Value want to Search");
+        int target = sc.nextInt();
+        System.out.println(search(arr, target, start, end));
     }
 }
