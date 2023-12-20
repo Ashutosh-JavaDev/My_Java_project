377a472f1d34ba0b140fe388c1f5d126dbf8c42e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-20 22:38:14 +0530

    Java Program

--- recursion/MaximumAndMinimum.java
+++ recursion/MaximumAndMinimum.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class MaximumAndMinimum {
-   public static int check()     
+   public static int check(int arr[],int size){
+    if(size==1){
+        return arr[0];
+    }
+    else{
+        return Math.min(arr[size-1], check(arr, size-1)); 
+    }
+   }     
+   public static void main(String[] args) {
+    
+   }
 }
