98189977e859048498e9d9ba31f95cb12fb3fd90
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-20 22:50:28 +0530

    Project

--- recursion/BinarySearch.java
+++ recursion/BinarySearch.java
@@ -17,6 +17,14 @@ public class BinarySearch {
         }
     }    
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array")
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter the Value want to Search")
     }
 }
