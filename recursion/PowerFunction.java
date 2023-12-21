0322747a63da91a14b5370290171a11300bf8efb
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 09:57:49 +0530

    Java Program

--- recursion/PowerFunction.java
+++ recursion/PowerFunction.java
@@ -1,18 +1,19 @@
 import java.util.*;
 public class PowerFunction {
-    public static int check(int num){
-        if(num==1){
+    public static int check(int num, int i){
+        if(i==num){
             return num;
         }
         else{
-            return num*check(num);
+            return num*check(num,i+1);
         }
     }    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int i=1;
         System.out.println("Enter the Term");
         int num=sc.nextInt();
-        int ans=check(num);
+        int ans=check(num,i);
         System.out.println(ans);
     }
 }
