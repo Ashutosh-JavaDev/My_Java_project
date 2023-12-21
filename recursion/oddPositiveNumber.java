f94d0e1abc074508f787416d3d67972e33a0aa70
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 10:04:53 +0530

    Java Program

--- recursion/oddPositiveNumber.java
+++ recursion/oddPositiveNumber.java
@@ -12,6 +12,8 @@ public class oddPositiveNumber {
     }    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("ENter the term");
+        int term=sc.nextInt();
         
     }
 }
