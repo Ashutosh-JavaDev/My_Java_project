587725777fd772e664c224c52b6d5b44eab31ca2
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 12:34:56 +0530

    Java Program

---
+++ recursion/oddEvenSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class oddEvenSum {
+    public static int add(int arr[],int size){
+        int sum=0;
+        if(size<=1){
+            return size;
+        }
+        else if(arr[size-1]%2==0){
+            return sum+add(arr,size-1); 
+        }
+        else{
+            return sum+add(arr, size-1);
+        }
+    }    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter Array Value");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int answer=add(arr, size);
+        System.out.println(answer);
+    }
+}
