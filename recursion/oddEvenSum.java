a1d3bd7a5d0a0cf3050ba0e747fc833d2721ca30
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 13:01:17 +0530

    Java Program

--- recursion/oddEvenSum.java
+++ recursion/oddEvenSum.java
@@ -1,27 +1,31 @@
 import java.util.*;
+
 public class oddEvenSum {
-    public static int add(int arr[],int size){
-        int even=0,odd=0;
-        if(size<=1){
-            return size;
+    public static int add(int arr[], int size) {
+        if (size <= 0) {
+            return 0;
         }
-        else if(arr[size-1]%2==0){
-            return even+add(arr,size-1); 
+
+        // int currentNumber = arr[size - 1];
+        // int recursiveSum = add(arr, size - 1);
+
+        if(arr[size-1]%2==0){
+            return add(arr, size-1)+arr[size-1];
+        } else {
+            return add(arr, size-1);
         }
-        else{
-            return odd+add(arr, size-1);
-        }
-    }    
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the value of array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        System.out.println("Enter Array Value");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter Array Values");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        int answer=add(arr, size);
-        System.out.println(answer);
+        int answer = add(arr, size);
+        System.out.println("Sum of even numbers: " + answer);
     }
 }
