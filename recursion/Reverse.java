71696feeabf7f9f792297d0f3a42d106d155d7ee
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 11:58:09 +0530

    Java Program

--- recursion/Reverse.java
+++ recursion/Reverse.java
@@ -10,6 +10,7 @@ public class Reverse {
             System.out.println(div);
             reverse(num/10);
         }
+        System.out.println();
 
     }    
     public static void main(String[] args) {
