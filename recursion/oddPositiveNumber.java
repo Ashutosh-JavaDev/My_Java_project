b8087fa5042722be85d04b305377029ad386a363
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-21 11:39:34 +0530

    Java Program

--- recursion/oddPositiveNumber.java
+++ recursion/oddPositiveNumber.java
@@ -1,19 +1,25 @@
 import java.util.*;
+
 public class oddPositiveNumber {
-    public static void check(int term,int i){
-        if(i>=term){
-            System.out.println(i);
+    public static void check(int term, int i) {
+        if (i > term) {
             return;
+        } else {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+            check(term, i + 2);
         }
-        else{
-            System.out.println(i);
-            check(term, i+2);
-        }
-    }    
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the term");
-        int term=sc.nextInt();
-        check(term, 1);
+        int term = sc.nextInt();
+        if (term <= 0) {
+            System.out.println("Please enter a positive term.");
+        } else {
+            check(term, 1);
+        }
     }
 }
