aa064cec6c4d05af25eaf00dae741dd02dda5f2b
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-22 15:08:03 +0530

    Java Program

--- recursion/PrintAllPossibleNumber.java
+++ recursion/PrintAllPossibleNumber.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class PrintAllPossibleNumber {
-    public static void possibleOutcome(int arr[],int target,int size){
-        
+    public static void possibleOutcome(int arr[],int target,int size,int i){
+        if(size<=0){
+            System.out.println(size);
+            return;
+        }
+        else if(arr[size-1]+arr[i+1]==target){
+            
+        }
     }    
 }
