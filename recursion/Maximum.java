0e35aa7b32650d6f9cabca96881d3e30e4caeb57
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-23 11:13:21 +0530

    Java Program

--- recursion/Maximum.java
+++ recursion/Maximum.java
@@ -1,29 +1,31 @@
 import java.util.*;
+
 public class Maximum {
     public static int check(int arr[],int size,int i){
-        int max=arr[size-1];
+   
         if(size<=0){
             return 0;
         }
-        else if(arr[i]>max){
-             max=arr[i];
-             return check(arr, size-1, i+1);
+             int max=check(arr, size-1, i+1);
+        if(arr[i]>max){
+            return arr[i];
         }
         else{
-            return check(arr, size-1, i+1);
+            return max;
         }
-    }    
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the Value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        int output=check(arr, size, 1);
-        System.out.println(output);
+        int output = check(arr, size, 0);
+        System.out.println("Max:    "+output);
     }
 
 }
