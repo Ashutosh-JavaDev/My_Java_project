c9f8065119bc7160b1acafd5c3305599bb89048b
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-23 15:46:54 +0530

    Java Program

--- recursion/fibonacci.java
+++ recursion/fibonacci.java
@@ -1,19 +1,20 @@
 import java.util.*;
+
 public class fibonacci {
-    public static int fibo(int num,int a,int b){
-        if(num<=1){
+    public static int fibo(int num, int a, int b) {
+        if (num < 1) {
             return num;
+        } else {
+            System.out.println(a);
+            int c = a + b;
+            return fibo(num - 1, b, c);
         }
-        else{
-            int c=a+b;
-            System.out.println(c);
-            return fibo(num, b, c);
-        }
-    }    
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter term");
-        int num=sc.nextInt();
+        int num = sc.nextInt();
         fibo(num, 0, 1);
     }
 }
