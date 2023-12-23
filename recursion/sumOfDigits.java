111a032fdf30bf555b99b2149d1f1b198e5d21db
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-23 16:29:42 +0530

    Java Program

--- recursion/sumOfDigits.java
+++ recursion/sumOfDigits.java
@@ -1,3 +1,14 @@
+import java.util.*;
 public class sumOfDigits {
-    
+    public static int sumOfdigits(int num){
+        int sum=0;
+        if(num<10){
+            return num;
+        }
+        else{
+            int d=num%10;
+            sum=sum+d;
+            return sumOfdigits(num/10);
+        }
+    }    
 }
