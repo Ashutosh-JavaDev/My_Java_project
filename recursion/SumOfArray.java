75f11c5c617c87805dcc7f8185215ab06f29a791
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-23 09:45:25 +0530

    Java Program

--- recursion/SumOfArray.java
+++ recursion/SumOfArray.java
@@ -13,5 +13,12 @@ public class SumOfArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int answer=total(arr, size);
     }
 }
