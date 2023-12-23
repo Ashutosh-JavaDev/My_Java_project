7dadca8322c7902dac17a07c47d8802685aa9830
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-23 09:42:35 +0530

    Java Program

---
+++ recursion/SumOfArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SumOfArray {
+    public static int total(int arr[],int size){
+        int sum=0;
+        if(size==0){
+            return 0;
+        }
+        else{
+            sum
+        }
+    }    
+}
