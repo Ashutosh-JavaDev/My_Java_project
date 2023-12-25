3c3aca8363b029d39665f3c4c221fde148beb00c
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-25 15:19:36 +0530

    Java Program

--- recursion/PrymidPatter.java
+++ recursion/PrymidPatter.java
@@ -13,4 +13,12 @@ public class PrymidPatter {
             pattern(i-1, 0);
         }
     }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of pattern");
+        int size=sc.nextInt();
+        pattern(size, 0);
+
+
+    }
 }
