4e04a2c0e60ccf114623e808bc99a174f15cd8d9
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-25 16:12:14 +0530

    Java Program

--- recursion/ReversePrymid.java
+++ recursion/ReversePrymid.java
@@ -1,3 +1,19 @@
+import java.util.*;
 public class ReversePrymid {
-    
+    public static void reversePattern(int i,int j){
+        if(i>5){
+            return;
+        }
+        if(i>j){
+            System.out.print("*");
+            reversePattern(i, j+1);
+        }
+        else{
+            System.out.println();
+            reversePattern(i+1, 0);
+        }
+    }    
+    public static void main(String[] args) {
+        reversePattern(1, 0);
+    }
 }
