790febc528f6deb64a8c0b63283ae6e4244ff89b
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-25 15:17:59 +0530

    Java Program

--- recursion/PrymidPatter.java
+++ recursion/PrymidPatter.java
@@ -1,4 +1,16 @@
 import java.util.*;
 public class PrymidPatter {
-    
+    public static void pattern(int i,int j){
+        if(i==j){
+            return;
+        }
+        if(j<i){
+            System.out.println("*");
+            pattern(i, j+1);
+        }
+        else{
+            System.out.println();
+            pattern(i-1, 0);
+        }
+    }
 }
