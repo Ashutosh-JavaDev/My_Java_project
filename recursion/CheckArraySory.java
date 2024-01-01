b45f30a91b9be8afbfa3b01faefd438bca8ccffb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-01 15:16:49 +0530

    Java Program

--- recursion/CheckArraySory.java
+++ recursion/CheckArraySory.java
@@ -4,8 +4,6 @@ class checksort {
 
     void arrayCheck(int arr[], int size) {
         int count = 0;
-        System.out.println("Ascending Array Count");
-
         if (size <= 0 || size == 1) {
             // System.out.println("Array is Not Sorted");
             return;
@@ -14,9 +12,13 @@ class checksort {
                 count++;
                 arrayCheck(arr, size - 1);
                 if (count == size - 1) {
+                    System.out.println("Ascending Array Count");
+
                     System.out.println("Array is Sorted");
                 }
             } else {
+                System.out.println("Ascending Array Count");
+
                 System.out.println("Array is Not Sorted");
             }
         }
@@ -26,19 +28,20 @@ class checksort {
     void arrayDescendingCheck(int arr[], int size, int i) {
         int count = 0;
         i = 0;
-        System.out.println("Descending Array Count");
 
-        if (i == size) {
+        if (i == size-1) {
             // System.out.println("Array is Not Sorted");
             return;
         } else {
             if (arr[i] < arr[i + 1]) {
                 count++;
                 arrayDescendingCheck(arr, size, i + 1);
-                if (count == size - 1) {
+                if (count == size ) {
+                    System.out.println("Descending Array Count");
                     System.out.println("Array is Sorted");
                 }
             } else {
+                System.out.println("Descending Array Count");
                 System.out.println("Array is Not Sorted");
             }
         }
@@ -47,14 +50,13 @@ class checksort {
 
 public class CheckArraySory {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 4, 6, 5 };
+        int arr[] = { 5,4,3,2,1};
         checksort ob = new checksort();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < arr[i + 1]) {
-                ob.arrayDescendingCheck(arr, arr.length, i);
-            } else {
-                ob.arrayCheck(arr, arr.length);
-            }
+        int i = 0;
+        if (arr[i] > arr[i + 1]) {
+            ob.arrayDescendingCheck(arr, arr.length, i);
+        } else {
+            ob.arrayCheck(arr, arr.length);
         }
 
     }
