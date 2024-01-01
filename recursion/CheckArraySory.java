ee26a9778c4bd9336b687f3ee399f55f5703f5a4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-01 14:53:32 +0530

    Java Program

--- recursion/CheckArraySory.java
+++ recursion/CheckArraySory.java
@@ -5,27 +5,25 @@ class checksort {
     void arrayCheck(int arr[], int size) {
         int count = 0;
 
-        if (size <= 0|| size ==1) {
-            System.out.println("Array is Sorted");
+        if (size <= 0 || size == 1) {
+            // System.out.println("Array is Not Sorted");
             return;
         } else {
             if (arr[size - 1] > arr[size - 2]) {
                 count++;
                 arrayCheck(arr, size - 1);
+                if (count == size - 1) {
+                    System.out.println("Array is Sorted");
+                }
             }
-        }
-        if (count == size-1) {
-            System.out.println("Array is Sorted");
-        } else {
-            System.out.println("Array is Not Sorted");
         }
     }
 }
 
 public class CheckArraySory {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        checksort ob=new checksort();
-        ob.arrayCheck(arr,arr.length);
+        int arr[] = { 1, 2, 3, 4, 5 };
+        checksort ob = new checksort();
+        ob.arrayCheck(arr, arr.length);
     }
 }
