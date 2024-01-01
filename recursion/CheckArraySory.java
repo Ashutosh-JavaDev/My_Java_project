6dde280711549d47792cf8f5fdb4c648e7beeacd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-01 14:40:41 +0530

    Java Program

--- recursion/CheckArraySory.java
+++ recursion/CheckArraySory.java
@@ -1,4 +1,28 @@
 import java.util.Scanner;
+
+class checksort {
+
+    int arrayCheck(int arr[],int size){
+                int count=0;
+
+        if(size<0){
+            return;
+        }
+        else{
+            if(arr[size-1]>arr[size-2]){
+                count++;
+             return arrayCheck(arr,size-1);
+            }
+        }
+        if(count == size){
+            System.out.println("Array is Sorted");
+        }
+        else{
+            System.out.println("Array is Not Sorted");
+        }
+    }
+}
+
 public class CheckArraySory {
-    
+
 }
