746fe184869765143b0ffd3517d4c27da44b7395
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-02 19:21:36 +0530

    Java Program

---
+++ recursion/BinarySearchTree.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class BinaryTree {
+    int search(int arr[], int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (s + e) / 2;
+
+        if (arr[m] == target) {
+            return m;
+        }
+        else if(target<arr[m]){
+           return search(arr, target, m-1, e);
+        }
+        else{
+           return search(arr, target, s, m+1);
+        }
+
+    }
+}
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        BinarySearch ob=new BinarySearch();
+        ob.search(arr, 8, 0, arr.length-1);
+    }
+}
