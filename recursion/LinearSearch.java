c25ac9ff2658244a309d8bff6ae6c9990e9030ca
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-02 11:22:27 +0530

    Java Program

--- recursion/LinearSearch.java
+++ recursion/LinearSearch.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 class recursive{
-    void  linear(int arr[],int target,int size){
-        
+   static int linear(int arr[],int target,int size){
+        if(size<0){
+            return -1;
+        }
+        else if (arr[size-1]==target){
+
+            return size;
+        }
+        return linear(arr, target, size-1);
     }
 }public class LinearSearch {
-    
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+    }
 }
