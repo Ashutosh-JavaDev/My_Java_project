ae164aa73c1d922759710a9424386cdb15e6de18
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-02 19:29:55 +0530

    Java Program

--- recursion/BinarySearchTree.java
+++ recursion/BinarySearchTree.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
+public class BinarySearchTree {
 
-class BinaryTree {
-    int search(int arr[], int target, int s, int e) {
+    public static int search(int arr[], int target, int s, int e) {
         if (s > e) {
             return -1;
-        }
-        int m = s + (s + e) / 2;
-
-        if (arr[m] == target) {
-            return m;
-        }
-        else if(target<arr[m]){
-           return search(arr, target, m-1, e);
-        }
-        else{
-           return search(arr, target, s, m+1);
+        } else {
+            int m = s + (e-s) / 2;
+            if (arr[m] == target) {
+                return m;
+            } else if (target < arr[m]) {
+                return search(arr, target, s, m-1);
+            } else {
+                return search(arr, target, m+1, e);
+            }
         }
 
     }
-}
 
-public class BinarySearchTree {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        BinarySearch ob=new BinarySearch();
-        ob.search(arr, 8, 0, arr.length-1);
+        int arr[] = { 1, 2, 3, 4, 5 };
+        // BinarySearch ob=new BinarySearch();
+       int result= search(arr, 2, 0, arr.length - 1);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
     }
 }
