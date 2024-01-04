24fdd1b8e0ff60a1194488398bbb4118de7f59aa
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 17:07:01 +0530

    Java Program

---
+++ Projects/funct.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class reverse {
+    String str;
+
+    void getString(String str) {
+        this.str=str;
+
+    }
+   /* String setString(){
+        return str;
+    }*/
+
+    String reverseString()
+     {
+        String st = "";
+        for (int i = (str.length() - 1); i >= 0; i--) {
+            st =st+ str.charAt(i);
+        }
+        return st;
+    }
+}
+
+public class funct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        reverse ob = new reverse();
+        ob.getString(sc.nextLine());
+        String result=ob.reverseString();
+        System.out.println(result);
+    }
+}
