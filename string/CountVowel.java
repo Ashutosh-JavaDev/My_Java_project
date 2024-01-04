7db863b23de771660a3915d901e7c9335bc9738c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 17:16:34 +0530

    Java Program

--- string/CountVowel.java
+++ string/CountVowel.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
-class Vowel{
+
+class Vowel {
     String Str;
-    int get(){
-        Scanner sc=new Scanner(System.in);
+
+    int get() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter String");
-        String str=sc.nextLine();
-        int count=0;
-        for(int i=0;i<str.length();i++){
-            if(str.equals("a")||str.equals("e")||str.equals("i")||str.equals("o")||str.equals("u")){
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
+                    || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
                 count++;
             }
         }
+        return count;
     }
-}public class CountVowel {
-    
+}
+
+public class CountVowel {
+    public static void main(String[] args) {
+        Vowel ob = new Vowel();
+        int result = ob.get();
+        System.out.println("No of Vowel:    " + result);
+    }
+
 }
