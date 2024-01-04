d282cf802a079c3bbc8b3238281d60bc54b2f8ac
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 17:22:30 +0530

    Java Program

---
+++ string/ReplaceDollar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ReplaceD {
+    String str;
+
+    void getString(String str) {
+        this.str = str;
+    }
+
+    void replaced() {
+        for (int i = 0; i < str.length(); i++) {
+            
+        }
+    }
+}
+
+public class ReplaceDollar {
+
+}
