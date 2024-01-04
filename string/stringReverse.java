2fc99e828a712fb0de44ed80c4621927a5669435
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 16:32:04 +0530

    Java Program

--- string/stringReverse.java
+++ string/stringReverse.java
@@ -3,26 +3,30 @@ import java.util.Scanner;
 class reverse {
     String str;
 
-    void getString() {
+    void getString(String str) {
+        this.str=str;
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter String");
-        str = sc.nextLine();
+    }
+    String setString(){
+        return str;
     }
 
     String reverseString() {
-        String st="";
-        for(int i=str.length()-1;i>=0;i--){
-            st=str+st;
+        String st = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            st = str.charAt(i) + st;
         }
         return st;
     }
 }
 
 public class stringReverse {
-public static void main(String[] args) {
-    reverse ob=new reverse();
-    ob.getString();
-    ob.reverseString();
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        reverse ob = new reverse();
+        ob.getString(sc.nextLine());
+        String result=ob.reverseString();
+        System.out.println(result);
+    }
 }
