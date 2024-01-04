99d2691886ac5c294b830b4bda72bedb3c51ac7f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 15:58:29 +0530

    Java Program

--- string/CountWord.java
+++ string/CountWord.java
@@ -10,16 +10,25 @@ class count {
     String set() {
         return str;
     }
-    void output(){
+    int output(){
         int c=0;
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)==' '){
                 c++;
             }
         }
+        return c;
     }
 }
 
 public class CountWord {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
+        count ob=new count();
+        ob.get(sc.nextLine());
+        int result=ob.output();
+        System.out.println("No. of  Words: "+(result+1));
 
+    }
 }
