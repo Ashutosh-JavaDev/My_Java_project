59b368383cd2806889a428868636898401a94b80
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-04 15:52:55 +0530

    Java Program

---
+++ string/CountWord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class count {
+    String str;
+
+    void get(String str) {
+        this.str = str;
+    }
+
+    String set() {
+        return str;
+    }
+}
+
+public class CountWord {
+
+}
