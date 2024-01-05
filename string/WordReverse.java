4ecec4abdbb7dfd2f4a0d97f4d642385dfa1c0cc
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 11:14:16 +0530

    Java Program

--- string/WordReverse.java
+++ string/WordReverse.java
@@ -8,6 +8,9 @@ class wordReverse {
         for (int i = str.length()-1; i >=0; i--) {
             if(str.charAt(i)!=' '){
                 st=st+str.charAt(i);
+                for(int j=str.length()-1;j>=0;j--){
+                    System.out.println(st.charAt(i));
+                }
             }
         }
     }
