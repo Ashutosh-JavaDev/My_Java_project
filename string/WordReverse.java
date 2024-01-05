22a446cefb57979b498b3ae7d5e9dfd1f9ed8ee6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 11:23:09 +0530

    Java Program

--- string/WordReverse.java
+++ string/WordReverse.java
@@ -6,12 +6,13 @@ class wordReverse {
 
     void wordRev() {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
         str=sc.nextLine();
         for (int i = str.length()-1; i >=0; i--) {
             if(str.charAt(i)!=' '){
                 st=st+str.charAt(i);
-                for(int j=str.length()-1;j>=0;j--){
-                    System.out.println(st.charAt(i));
+                for(int j=0;j<st.length();j++){
+                    System.out.println(st);
                 }
             }
         }
