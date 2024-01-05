fc6cea676c062a35fdee76901beaade2a36dd3dd
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-05 12:23:38 +0530

    Java Program

--- string/WordReverse.java
+++ string/WordReverse.java
@@ -1,27 +1,30 @@
 import java.util.Scanner;
 
 class wordReverse {
-    String str,st="";
+    String str, st = "", revSt = "";
     char ch;
 
     void wordRev() {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter String");
-        str=sc.nextLine();
-        for (int i = str.length()-1; i >=0; i--) {
-            if(str.charAt(i)!=' '){
-                st=st+str.charAt(i);
-                for(int j=0;j<st.length();j++){
-                    System.out.println(st);
-                }
+        str = sc.nextLine();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) != ' ') {
+                st = st + str.charAt(i);
+            } else {
+                revSt += st + " ";
+                st="";
             }
         }
+        revSt+=st+"";
+        System.out.println(revSt.trim());
+
     }
 }
 
 public class WordReverse {
-    public static void main(String[]args){
-        wordReverse ob=new wordReverse();
+    public static void main(String[] args) {
+        wordReverse ob = new wordReverse();
         ob.wordRev();
     }
 }
