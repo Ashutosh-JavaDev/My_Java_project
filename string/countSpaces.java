6adbd9a784d7a96f075e0878d466b0be6456456f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 12:08:08 +0530

    Java Program

--- string/countSpaces.java
+++ string/countSpaces.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
-class spacesCount{
-    void splitsSpaces()
+
+class spacesCount {
+    void splitsSpaces(String str){
+        int c=1;
+        char ch[]=str.toCharArray();
+        for(int i=0;i<str.length();i++){
+               if(ch[i].equals('') && ch[i+1]!=' '){
+                c++;
+               }       
+            
+        }
+        System.out.println("Count of Words: "+c);
+
+    }
 }
+
 public class countSpaces {
-    
+
 }
