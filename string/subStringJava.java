8e47c53810c12940a0f3f5da868f5d533500cb0c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 12:48:04 +0530

    Java Program

--- string/subStringJava.java
+++ string/subStringJava.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
-class subStr{
-    void stringCheck(String str){
-        int c=0;
-        char ch[]=str.toCharArray();
-        for(int i=0;i<str.length();i++){
-           if(str.charAt(i)=='h' && str.charAt(i+1)=='i' && str.charAt(i+2)==' '){
-            c++;
-           }
+
+class subStr {
+    void stringCheck(String str) {
+        int c = 0;
+        char ch[] = str.toCharArray();
+        String [] words;
+        for (int i = 0; i < str.length(); i++) {
+        words =str.split(" ");
         }
-        System.out.println("Total hi in the String: "+c);
+        System.out.println(words);
+        // System.out.println("Total hi in the String: " + c);
     }
 }
+
 public class subStringJava {
-    public static void main(String[]args){
-        subStr ob=new subStr();
+    public static void main(String[] args) {
+        subStr ob = new subStr();
         ob.stringCheck("Hi Everyone how hi are hi this you his");
     }
 }
