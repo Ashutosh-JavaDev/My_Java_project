509e178cb21f95f9ef0211212516c646cef91340
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 16:28:20 +0530

    Java Program

--- string/deleteAllVowel.java
+++ string/deleteAllVowel.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
-class vowelCheck{
-    void disp(String str){
+
+class vowelCheck {
+    void disp(String str) {
         System.out.println("Deleted Vowel");
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='a' || str.charAt(i)=='o' ||str.charAt(i)=='i'||str.charAt(i)=='e'||str.charAt(i)=='u'){
-                char ch=str.charAt(i);
-                System.out.print(ch+" ");
+        String str1 = " ";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'o' || str.charAt(i) == 'i' || str.charAt(i) == 'e'
+                    || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+                char ch = str.charAt(i);
+                System.out.print(ch + " ");
+            } else {
+                str1 += str.charAt(i);
             }
         }
-    }    
+        System.out.println();
+        System.out.println("New String: " + str1);
+    }
 }
+
 public class deleteAllVowel {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        vowelCheck ob=new vowelCheck();
-        ob.disp("apple");
+        Scanner sc = new Scanner(System.in);
+        vowelCheck ob = new vowelCheck();
+        System.out.println("Pass String");
+        ob.disp(sc.nextLine());
 
     }
 }
