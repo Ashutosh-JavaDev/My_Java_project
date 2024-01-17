0de6c1da718b332d2eae4236244cdbcc060bb906
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 10:43:13 +0530

    Java Program

--- string/Palindrome.java
+++ string/Palindrome.java
@@ -5,12 +5,17 @@ class Check {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String");
         str = sc.nextLine();
-        String[] newString;
         char ch;
         String sp = "";
         for (int i = str.length() - 1; i >= 0; i--) {
             ch = str.charAt(i);
-
+            sp+=ch;
+        }
+        if(sp.equals(str)){
+            System.out.println("String is Palindrome");
+        }
+        else{
+            System.out.println("String ids not palindrome");
         }
     }
 }
