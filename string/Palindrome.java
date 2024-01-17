f7ed1d4b0e7aae4cb189f123d728f700f61dca3d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 10:40:25 +0530

    Java Program

---
+++ string/Palindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Check{
+    void palin(String str){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String");
+        str=sc.nextLine();
+        String []newString;
+        char ch;
+        String sp="";
+        for(int i=str.length()-1;i>=0;i--){
+        ch=str.charAt(i);
+
+        }
+    }
+}public class Palindrome {
+    
+}
