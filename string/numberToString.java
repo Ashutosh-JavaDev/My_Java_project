dfc988495f8ce97c7a37077d44b1a2b4df25fdb9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 15:21:50 +0530

    Java Program

---
+++ string/numberToString.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class numTochar{
+    void disp(String num){
+        String st1=" ";
+        char ch;
+       for(int i=0;i<num.length();i++){
+        ch=num.charAt(i);
+        switch(ch){
+            case '1':
+            st1+="One";
+            break;
+            case '2':
+            st1+="Two";
+            break;
+            case '3':
+            st1+="Three";
+            break;
+            case '4':
+            st1+="Four";
+            break;
+            case '5':
+            st1+="Five";
+            break;
+            case '6':
+            st1+="Six";
+            break;
+            case '7':
+            st1+="Seven";
+            break;
+            case '8':
+            st1+="Eight";
+            break;
+            case '9':
+            st1+="Nine";
+            break;
+            default:
+            System.out.println("Invalid Press");
+            break;
+        }
+       }
+       System.out.println("In String");
+    }
+}
+public class numberToString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Press Number");
+        numTochar ob=new numTochar();
+        ob.disp(sc.nextLine());
+    }
+}
