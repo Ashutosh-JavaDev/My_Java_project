1a9af0fa873693e2c343ecec789e0ba43bfb1130
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 16:12:26 +0530

    Java Program

--- string/deleteAllVowel.java
+++ string/deleteAllVowel.java
@@ -5,7 +5,7 @@ class vowelCheck{
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)=='a' || str.charAt(i)=='o' ||str.charAt(i)=='i'||str.charAt(i)=='e'||str.charAt(i)=='u'){
                 char ch=str.charAt(i);
-                System.out.println(ch);
+                System.out.print(ch+" ");
             }
         }
     }    
