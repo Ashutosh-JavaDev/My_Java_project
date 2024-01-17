0f1f43731476da10dfb62bcd4f5312de2ac21df9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 14:31:34 +0530

    Java Program

--- string/subStringJava.java
+++ string/subStringJava.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 class subStr {
     void stringCheck(String str) {
-      
+        int c = 0;
+        char ch[] = str.toCharArray();
+        String words[] = str.split(" ");
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = i + 1; j < ch.length; j++) {
+                if (words[i].equalsIgnoreCase(words[j])) {
+
+                }
+            }
+        }
     }
 }
 
@@ -12,4 +21,3 @@ public class subStringJava {
         ob.stringCheck("Hi Everyone how hi are hi this you his");
     }
 }
-
