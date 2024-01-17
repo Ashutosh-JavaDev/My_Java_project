14092ae570e131a289d0cfa36ab2da89a83deabe
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 14:41:59 +0530

    Java Program

--- string/subStringJava.java
+++ string/subStringJava.java
@@ -2,25 +2,20 @@ import java.util.Scanner;
 
 class subStr {
     void stringCheck(String str) {
-        int c = 1;
-        char ch[] = str.toCharArray();
+        int c;
+        // char ch[] = str.toCharArray();
         String words[] = str.split(" ");
-        for (int i = 0; i < ch.length; i++) {
-            for (int j = i + 1; j < ch.length; j++) {
+        for (int i = 0; i < words.length; i++) {
+            c=1;
+            for (int j = i + 1; j < words.length; j++) {
                 if (words[i].equalsIgnoreCase(words[j])) {
                     c++;
                     words[j]="0";
                 }
             }
             if(c>1 && words[i]!="0"){
-                System.out.println(words[i]);
+                System.out.println(words[i]+" - Duplicates Count:   "+c);
             }
-        }
-        if(c>1){
-            System.out.println("Duplicate words:    "+c);
-        }
-        else{
-            System.out.println("No Duplicate Words Found");
         }
     }
 }
@@ -28,6 +23,6 @@ class subStr {
 public class subStringJava {
     public static void main(String[] args) {
         subStr ob = new subStr();
-        ob.stringCheck("Hi Everyone how hi how this how are hi this you his");
+        ob.stringCheck("hii Every Hii");
     }
 }
