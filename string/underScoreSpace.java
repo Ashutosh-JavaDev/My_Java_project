ed6d22a251f7785a9dce004f4c3f50f39a9a6808
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-17 16:32:36 +0530

    Java Program

---
+++ string/underScoreSpace.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class replaceUnderscore{
+    void checkDisp(String str){
+        String newString=str.replace(" ", "_");
+    }    
+}
+public class underScoreSpace {
+    
+}
