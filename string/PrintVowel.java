bb037d94afd9bc6132a6ab026ebc238212165182
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 16:09:25 +0530

    resursion

--- string/PrintVowel.java
+++ string/PrintVowel.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
-class vowelPrint{
-    void printvowel(String str){
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                char newch=ch; 
+
+class vowelPrint {
+    void printvowel(String str) {
+        String st = " ";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                char newch = ch;
+            } else {
+                st = st + ch;
             }
         }
     }
 }
+
 public class PrintVowel {
-    
+
 }
