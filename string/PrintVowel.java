7c33f77824d70e1ef948ac5253701f9e2d52e044
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-23 16:23:20 +0530

    String Program

--- string/PrintVowel.java
+++ string/PrintVowel.java
@@ -5,22 +5,23 @@ class vowelPrint {
         String st = " ";
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                    || ch == 'O' || ch == 'U') {
                 char newch = ch;
             } else {
                 st = st + ch;
             }
         }
-        System.out.println("New String: "+st);
+        System.out.println("New String: " + st);
     }
 }
 
 public class PrintVowel {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        vowelPrint ob=new vowelPrint();
+        Scanner sc = new Scanner(System.in);
+        vowelPrint ob = new vowelPrint();
         System.out.print("Enter String: ");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
         ob.printvowel(str);
     }
 }
