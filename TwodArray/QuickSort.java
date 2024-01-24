2c011a1db7aed9f71784c64291137c093c8191ce
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-24 22:07:09 +0530

    Program

--- TwodArray/QuickSort.java
+++ TwodArray/QuickSort.java
@@ -1,3 +1,25 @@
-public class QuickSort {
+class quick{
+    int quickSorting(int arr[],int low,int high){
+        int pivot=high;
+        int i=low-1;
+        for(int j=0;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        return i;
+    }
+    void recursiveQuick(int arr[],int low,int high){
+
+        if(low<high){
+            int recInd=quickSorting(arr,low,high);
+            recursiveQuick(arr, recInd+1, high);
+            recursiveQuick(arr, low, recInd);
+        }
+    }
+}public class QuickSort {
     
 }
