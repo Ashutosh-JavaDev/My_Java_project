2b7f52a36c75ac68c7b49cf9b6a24e620172d993
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 21:43:43 +0530

    Program

---
+++ recursion/reverseTriangle.java
@@ -0,0 +1,11 @@
+class revTriangle{
+    void revTri(int i,int k){
+
+    }
+}
+public class reverseTriangle {
+    public static void main(String[] args) {
+        revTriangle ob=new revTriangle();
+        ob.revTri(4, 5);
+    }
+}
