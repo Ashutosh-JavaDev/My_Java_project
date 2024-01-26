9930b110bd9ea01d7bd795efded5c6ee378d7ca4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 21:13:26 +0530

    Loop Program

--- recursion/printTriangle.java
+++ recursion/printTriangle.java
@@ -1,26 +1,67 @@
-import java.util.Scanner;
+// // package Loop;
 
-class Triangle {
-    void TrianglePrint(int i, int j) {
-        if (i > 5) {
-            return;
-        } else if (i <= 5) {
-            if (j <= i * 2 - 1) {
-                System.out.print("* ");
-                System.out.println();
+// class Triangle {
+//     void printSpaces(int space) {
+//         if (space > 0) {
+//             System.out.print(" ");
+//             printSpaces(space - 1);
+//         }
+//     }
 
-                TrianglePrint(i, j + 1);
-            } else {
-                TrianglePrint(i + 1, 1);
-            }
+//     void printStars(int stars) {
+//         if (stars > 0) {
+//             System.out.print("*");
+//             printStars(stars - 1);
+//         }
+//     }
 
+//     void TrianglePrint(int i, int k) {
+//         if (i > 5) {
+//             return;
+//         } else if (i <= 5) {
+//             printSpaces(k);
+//             printStars(i * 2 - 1);
+//             System.out.println();
+//             TrianglePrint(i + 1, k - 1);
+//         }
+//     }
+// }
+
+// public class printTriangle {
+//     public static void main(String[] args) {
+//         Triangle ob = new Triangle();
+//         ob.TrianglePrint(1, 5);
+//     }
+// }
+class Asterik{
+    void printSpace(int space){
+        if(space>0){
+            System.out.print(" ");
+            printSpace(space-1);
         }
     }
-}
-
-public class printTriangle {
-    public static void main(String[] args) {
-        Triangle ob = new Triangle();
-        ob.TrianglePrint(1, 1);
+    void printStar(int star){
+        if(star>0){
+            System.out.print("*");
+            printStar(star-1);
+        }
     }
-}
+    void asterikPattern(int i,int k){
+        if(i>5){
+            return;
+        }
+        else if(i<=5){
+                printSpace(k);
+                printStar(i*2-1);
+                System.out.println();
+                asterikPattern(i+1, k-1);
+
+            }
+        }
+    }
+    public class printTriangle{
+        public static void main(String[] args) {
+            Asterik ob=new Asterik();
+            ob.asterikPattern(1, 5);
+        }
+    }
