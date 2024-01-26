27453fcfb0a25c6476c5966e3726c611f5456a8f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 21:38:13 +0530

    Program

--- recursion/printTriangle.java
+++ recursion/printTriangle.java
@@ -1,38 +1,3 @@
-// // package Loop;
-
-// class Triangle {
-//     void printSpaces(int space) {
-//         if (space > 0) {
-//             System.out.print(" ");
-//             printSpaces(space - 1);
-//         }
-//     }
-
-//     void printStars(int stars) {
-//         if (stars > 0) {
-//             System.out.print("*");
-//             printStars(stars - 1);
-//         }
-//     }
-
-//     void TrianglePrint(int i, int k) {
-//         if (i > 5) {
-//             return;
-//         } else if (i <= 5) {
-//             printSpaces(k);
-//             printStars(i * 2 - 1);
-//             System.out.println();
-//             TrianglePrint(i + 1, k - 1);
-//         }
-//     }
-// }
-
-// public class printTriangle {
-//     public static void main(String[] args) {
-//         Triangle ob = new Triangle();
-//         ob.TrianglePrint(1, 5);
-//     }
-// }
 class Asterik{
     void printSpace(int space){
         if(space>0){
@@ -58,7 +23,6 @@ class Asterik{
 
             }
         }
-
     }
     public class printTriangle{
         public static void main(String[] args) {
