7e2dff8d6fa8020b5716c2a252fa4770808786f7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-12 14:46:35 +0530

    Diamond

--- my_practice/Diamond.java
+++ my_practice/Diamond.java
@@ -1,18 +1,22 @@
 public class Diamond {
     public static void main(String[] args) {
-            for(int i=1;i<=5;i++){
-                for(int k=4;k>=i;k--){
-                    System.out.print(" ");
-                }
-                for(int j=1;j<i*2;j++){
-                    if(i+j%2==0){
-                    System.out.print("*");
-                    }
-                    else{
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int k = 4; k >= i; k--) {
+                System.out.print(" ");
             }
+            for (int j = 1; j < i * 2; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 4; i >= 1; i--) {
+            for (int k = 4; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < i * 2; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
