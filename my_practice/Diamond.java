eeaa0cba81560faddfd7e03db0c794fb8d4fdd3f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-22 23:05:00 +0530

    Project

--- my_practice/Diamond.java
+++ my_practice/Diamond.java
@@ -13,7 +13,7 @@ public class Diamond {
             for (int k = 4; k >= i; k--) {
                 System.out.print(" ");
             }
-//             for (int j = 1; j < i * 2; j++) {
+            for (int j = 1; j < i * 2; j++) {
 //                 System.out.print("*");
 //             }
 //             System.out.println();
