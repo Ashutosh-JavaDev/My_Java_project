690dbdd37db0635684bae5b296f8a9fc71bb0ab9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-13 15:37:27 +0530

    Project

--- my_practice/reverse.java
+++ my_practice/reverse.java
@@ -1,20 +1,19 @@
 //programm will take input from the user and  diplay the reverse output to the user
-import java.util.*;
+//1+4/3+7/9+10/27
+
 public class reverse{
-	public static void main(String[]args){
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number for reverse");
-		int a=sc.nextInt();
-		reverse(a);
+	public static void main(String[]args){		
+		int result=sum();
+		System.out.println("Result: "+result);
 	}
-	public static void reverse(int num){
-		int rev=0;
-		while(num>0){
-			int d=num%10;
-			 rev=rev*10+d;
-			num=num/10;
+	public static int sum(){
+		int add=0;
+		for(int i=100;i<=999;i++){
+			if(i%7==0){
+				add+=i;
+			}
 		}
-		System.out.println("output : "+rev);
+		return add;
 		
 	}
 }
