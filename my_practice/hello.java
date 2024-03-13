7f0ea4b23b8e5b738811d2fe7f0cd11c57e92be1
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-13 15:41:07 +0530

    Project

--- my_practice/hello.java
+++ my_practice/hello.java
@@ -1,6 +1,21 @@
 import java.util.*;
+class data {
+	int sum() {
+		int add = 0;
+		for (int i = 100; i <= 999; i++) {
+			if (i % 7 == 0) {
+				add += i;
+			}
+		}
+		return add;
+
+	}
+}
+
 public class hello{
 	public static void main(String[]args){
-		System.out.println("hello world");
+		data ob=new data();
+		int result=ob.sum();
+		System.out.println("Result: "+result);
 	}
 }
