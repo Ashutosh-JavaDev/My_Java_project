d8a17499300ba82d0a789f0b60b1397e6d924e91
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-29 23:54:25 +0530

    Java Program

--- my_practice/hello.java
+++ my_practice/hello.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 class sorting{
 	public void sort(int arr[],int size){
-		
+		for(int i=0;i<size-1;i++){
+			for(int j=i+1;j<size-i-1;j++){
+				
+			}
+		}
 	}
 }
 public class hello{
