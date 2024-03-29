8b9f623e301337efe3b9d2fab9ab210986fab85d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-29 23:59:37 +0530

    Java Program

--- my_practice/hello.java
+++ my_practice/hello.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class sorting{
 	public void sort(int arr[],int size){
 		for(int i=0;i<size-1;i++){
-			for(int j=i+1;j<size-i-1;j++){
+			for(int j=i+1;j<size;j++){
 				if(arr[i]>arr[j]){
 					int temp=arr[i];
 					arr[i]=arr[j];
