5b0f499140de252cb77fdc51914a9dc4a3a3a08e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-29 23:53:11 +0530

    Java Program

--- my_practice/hello.java
+++ my_practice/hello.java
@@ -1,21 +1,9 @@
-import java.util.*;
-class data {
-	int sum() {
-		int add = 0;
-		for (int i = 100; i <= 999; i++) {
-			if (i % 7 == 0) {
-				add += i;
-			}
-		}
-		return add;
-
+import java.util.Scanner;
+class sorting{
+	public void sort(int arr[],int size){
+		
 	}
 }
-
 public class hello{
-	public static void main(String[]args){
-		data ob=new data();
-		int result=ob.sum();
-		System.out.println("Result: "+result);
-	}
+	
 }
