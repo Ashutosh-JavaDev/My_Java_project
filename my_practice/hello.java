5d0f09e5cd18b8e02efb626cf1487047490739bf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-03-29 23:55:14 +0530

    Java Program

--- my_practice/hello.java
+++ my_practice/hello.java
@@ -3,7 +3,11 @@ class sorting{
 	public void sort(int arr[],int size){
 		for(int i=0;i<size-1;i++){
 			for(int j=i+1;j<size-i-1;j++){
-				
+				if(arr[i]>arr[j]){
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
 			}
 		}
 	}
