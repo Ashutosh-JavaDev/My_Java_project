f017d05a33a7d46476f6301b103cd78db02623c0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-03 09:38:20 +0530

    Web Project

--- OOPS/Password.java
+++ OOPS/Password.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-class password{
-    public static String writePassword(String create,String newCreate){
-        Scanner scc=new Scanner(System.in);
-        System.out.println("Create Your Password");
 
-    String pass="";
+class password {
+    public static String writePassword(String create, String newCreate) {
+        Scanner scc = new Scanner(System.in);
+        System.out.print("Create Your Password: ");
+        create = scc.nextLine();
+
+        String pass = "";
 
         return pass;
     }
 }
+
 public class Password {
-    
+
 }
