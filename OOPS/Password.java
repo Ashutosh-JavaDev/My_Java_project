4808d002fcc9a6c514bccbe0759e9583d892483f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-03 09:41:24 +0530

    Java Program

--- OOPS/Password.java
+++ OOPS/Password.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 class password {
     public static String writePassword(String create, String newCreate) {
         Scanner scc = new Scanner(System.in);
+        System.out.println("Password must Contain a UpperCase, a LowerCase, Numeric Digit, Specific Symbols");
         System.out.print("Create Your Password: ");
         create = scc.nextLine();
-
+        System.out.print("Re-Enter Your Password: ");
+        newCreate=scc.nextLine();
         String pass = "";
 
         return pass;
