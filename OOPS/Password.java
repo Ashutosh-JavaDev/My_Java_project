29c4e8cbf839d86314f0b612ad9dda6d5cb39f28
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-03 09:49:33 +0530

    Java Program

--- OOPS/Password.java
+++ OOPS/Password.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 class password {
     public static String writePassword(String create, String newCreate) {
         Scanner scc = new Scanner(System.in);
-        System.out.println("Password must Contain a UpperCase, a LowerCase, Numeric Digit, Specific Symbols");
+        System.out.println("Password must Contain a UpperCase, a LowerCase, Numeric Digit, Specific Symbols,should be 8-16 letters.");
         System.out.print("Create Your Password: ");
         create = scc.nextLine();
+        int len=create.length();
+        if(len>=8 && len<=16){
+            for(int i=0;i<len;i++){
+                char ch=create.charAt(i);
+            }
+        }
+        else{
+            System.out.println("Password must contains 8-16 words.");
+        }
         System.out.print("Re-Enter Your Password: ");
         newCreate=scc.nextLine();
         String pass = "";
