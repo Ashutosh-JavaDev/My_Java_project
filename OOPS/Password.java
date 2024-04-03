7ffd381f54f49b4b82b67321a32757b8d2ef1078
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-03 10:23:31 +0530

    Java Program

--- OOPS/Password.java
+++ OOPS/Password.java
@@ -13,13 +13,28 @@ class password {
         System.out.print("Create Your Password: ");
         create = scc.nextLine();
         int len = create.length();
+        int upper = 0;
+        int lower = 0;
+        int Specific = 0;
+        int digit = 0;
+
         while (true) {
             if (len >= 8 && len <= 25) {
                 for (int i = 0; i < len; i++) {
                     char ch = create.charAt(i);
                     Ascii = (int) ch;
+                    if (Ascii >= 64 && Ascii <= 90) {
+                        upper++;
+                    } else if (Ascii >= 97 && Ascii <= 122) {
+                        lower++;
+                    } else if (Ascii >= 48 && Ascii <= 57) {
+                        digit++;
+                    } else if ((Ascii >= 33 && Ascii <= 47) || (Ascii >= 58 && Ascii <= 64)
+                            || (Ascii >= 91 && Ascii <= 96) || (Ascii >= 123 && Ascii <= 126)) {
+                        Specific++;
+                    }
                 }
-                if (Ascii >= 64 && Ascii <= 90 && Ascii >= 97 && Ascii <= 122 && Ascii == 32) {
+                if (upper > 0 && lower > 0 && digit > 0 && Specific > 0) {
                     System.out.println("Password Contains all the Criteria.");
                     System.out.print("Re-Enter Your Password: ");
                     newCreate = scc.nextLine();
@@ -30,7 +45,8 @@ class password {
                         break;
                     }
                 } else {
-                    System.out.println("Password must Contain a UpperCase, a LowerCase, Numeric Digit, Specific Symbols,should be 8-16 letters.");
+                    System.out.println(
+                            "Password must Contain a UpperCase, a LowerCase, Numeric Digit, Specific Symbols,should be 8-25 letters.");
                     break;
                 }
 
