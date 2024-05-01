26ae46355d21c30dc2c3693a877bcddda592144e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-01 16:31:50 +0530

    String Program

--- string/Palindrome.java
+++ string/Palindrome.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 class palin{
     boolean check(String str){
         boolean result=true;
-        
+        for(int i=str.length();i<=0;i--){
+            char ch=str.charAt(i);
+        }
         return result;
     }
 }
