27277ff182460d9fcc81f58b5d820c14cbdeb68f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-01 17:45:39 +0530

    String

--- string/spaceRemove.java
+++ string/spaceRemove.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
-class remSpace{
-    void sapceRem(String str){
-        
+
+class remSpace {
+    void sapceRem(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') {
+
+            }
+        }
     }
 }
+
 public class spaceRemove {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        remSpace ob=new remSpace();
+        Scanner sc = new Scanner(System.in);
+        remSpace ob = new remSpace();
         System.out.println("Enter the String");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
         ob.sapceRem(str);
     }
 }
