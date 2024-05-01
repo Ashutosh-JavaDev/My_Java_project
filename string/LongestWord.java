2495d799043be047bf5aa199e13b9502076018b9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-01 18:15:22 +0530

    String

--- string/LongestWord.java
+++ string/LongestWord.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
-class longword{
-    String lingWord(String str){
-        String result="";
-        
-        return result;
+
+class longword {
+    String lingWord(String str) {
+
+        String longst = "";
+        String shortst = "";
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != ' ') {
+                shortst += ch;
+            } else {
+                if (shortst.length() > max) {
+                    max = shortst.length();
+                    longst = shortst;
+                }
+                shortst = " ";
+            }
+        }
+        if (shortst.length() > max) {
+            longst = shortst;
+        }
+        return longst;
     }
 }
+
 public class LongestWord {
-public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-}    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
