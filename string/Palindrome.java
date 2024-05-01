7dce1ef1dad4d995b59bec97cc74201edd57c14d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-01 16:29:34 +0530

    String Program

---
+++ string/Palindrome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class palin{
+    boolean check(String str){
+        boolean result=true;
+        
+        return result;
+    }
+}
+public class Palindrome {
+    public static void main(String[] args) {
+        
+    }
+}
