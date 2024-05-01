11738cc6adc71a561d6527b4c870228ef614bfd8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-01 17:47:06 +0530

    String

--- string/spaceRemove.java
+++ string/spaceRemove.java
@@ -4,8 +4,11 @@ class remSpace {
     void sapceRem(String str) {
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if (ch == ' ') {
-
+            if (ch != ' ') {
+                System.out.print(ch);
+            }
+            else{
+                i++;
             }
         }
     }
