0ceef0450ecbcd7b2e3d94ee770f0b980b71550b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-02 23:16:22 +0530

    String Program

--- string/remDupli.java
+++ string/remDupli.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
-class removeDupli{
-    void check(String str){
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            
+
+class removeDupli {
+    void check(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+
+            }
+
         }
     }
 }
+
 public class remDupli {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        removeDupli ob=new removeDupli();
+        Scanner sc = new Scanner(System.in);
+        removeDupli ob = new removeDupli();
         System.out.println("Enter the String");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
         ob.check(str);
     }
 }
