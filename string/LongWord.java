2bd655a824905e9047c3459476e793769f58ffd8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-06 20:59:46 +0530

    String Program

--- string/LongWord.java
+++ string/LongWord.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
-class lonngwrd{
-   String check(String str){
-        String longest="";
-        String shortest="";
-        int max=0;
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(ch!=' '){
-                shortest+=ch;
-            }
-            else{
-                if(shortest.length()>max){
-                    max=shortest.length();
-                    longest=shortest;
+
+class lonngwrd {
+    String check(String str) {
+        String longest = "";
+        String shortest = "";
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch != ' ') {
+                shortest += ch;
+            } else {
+                if (shortest.length() > max) {
+                    max = shortest.length();
+                    longest = shortest;
                 }
+                shortest = " ";
+
             }
-            shortest=" ";
         }
-        if(shortest.length()>max){
-            longest=shortest;
+        if (shortest.length() > max) {
+            longest = shortest;
         }
         return longest;
     }
 }
+
 public class LongWord {
     public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
-         System.out.println("Enter the String");
-         String str=sc.nextLine();
-         lonngwrd ob=new lonngwrd();
-         String res=ob.check(str);
-         System.out.println("Longest Word: "+res);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str = sc.nextLine();
+        lonngwrd ob = new lonngwrd();
+        String res = ob.check(str);
+        System.out.println("Longest Word: " + res);
 
     }
 }
