cdb44c3a1ad8dfb80fe2cf624f1d43f311b4fe3e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-06 20:51:10 +0530

    String

--- string/LongWord.java
+++ string/LongWord.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 class lonngwrd{
-
+    public static String check(String str){
+        String longest="";
+        String shortest="";
+        int max=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch!=' '){
+                
+            }
+        }
+        return longest;
+    }
 }
 public class LongWord {
-    
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the String");
+         String str=sc.nextLine();
+         lonngwrd ob=new lonngwrd();
+         String res=ob.check(str);
+         System.out.println("Longest Word: "+res);
+
+    }
 }
