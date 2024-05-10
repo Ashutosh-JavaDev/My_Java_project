3a3af2a6edbfb7aaf87f83be8fb0ac0b7984ea2f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-10 17:39:11 +0530

    Maximum SubString

--- Projects/LongestPalindromeSunstring.java
+++ Projects/LongestPalindromeSunstring.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class LongestPalindromeSunstring {
     public static String MaximumString(String str[], int size) {
-        String maximum="";
-        int currentMax=0;
-        int max=0;
+        String maximum = "";
+        String st="";
+        int currentMax = 0;
+        int max = 0;
         for (int i = 0; i < size; i++) {
-            for(int j=str[i].length()-1;j>=0;j--){
-                
+            for (int j = str[i].length() - 1; j >= 0; j--) {
+                char ch=str[i].charAt(i);
+                st+=ch;
+                if(str[j].equalsIgnoreCase(st)){
+                    
+                }
             }
         }
         return maximum;
