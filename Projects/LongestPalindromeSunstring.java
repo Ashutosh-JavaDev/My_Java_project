0f8e66ae0659a6ae5d89e8cd474a3a83d268999c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-10 17:40:29 +0530

    Maximum SubString

--- Projects/LongestPalindromeSunstring.java
+++ Projects/LongestPalindromeSunstring.java
@@ -16,6 +16,7 @@ public class LongestPalindromeSunstring {
                         max = currentMax;
                         maximum = str[i];
                     }
+                    currentMax=0;
                 }
             }
         }
