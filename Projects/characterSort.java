f867fbc8aa497f234d1d4ed54ab0261b36082af5
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-10 16:47:48 +0530

    String Sort

--- Projects/characterSort.java
+++ Projects/characterSort.java
@@ -1,6 +1,10 @@
 public class characterSort {
-    public static String input(String value){
-        char[]charArray=value.toCharArray();
-        return new String (charArray);
+    public static String input(String value) {
+        char[] charArray = value.toCharArray();
+        for (int i = 0; i < charArray.length - 1; i++)
+            for (int j = 0; j < charArray.length - i - 1; j++) {
+
+            }
+        return new String(charArray);
     }
 }
