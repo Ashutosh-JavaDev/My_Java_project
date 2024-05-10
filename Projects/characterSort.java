3b1bfad2146c0acfc2738b9dd539c34685dc0685
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-10 16:50:04 +0530

    String Sort

--- Projects/characterSort.java
+++ Projects/characterSort.java
@@ -3,7 +3,11 @@ public class characterSort {
         char[] charArray = value.toCharArray();
         for (int i = 0; i < charArray.length - 1; i++)
             for (int j = 0; j < charArray.length - i - 1; j++) {
-
+                if (charArray[j] < charArray[j + 1]) {
+                    char temp = charArray[j];
+                    charArray[j] = charArray[j + 1];
+                    charArray[j + 1] = temp;
+                }
             }
         return new String(charArray);
     }
