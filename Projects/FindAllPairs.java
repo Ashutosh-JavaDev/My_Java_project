6a08449aa2f13006e46dab30d0a36ebd9b428bce
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-10 16:03:53 +0530

    Array Program

--- Projects/FindAllPairs.java
+++ Projects/FindAllPairs.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 class findsPairs{
-    
+    public static void selectPairs(int arr[],int size,int total){
+        int count=0;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]+arr[j]==total){
+                    System.out.println("arr[ "+i+" ]+ arr[ " +j+ "] = "+total);
+                    count++;
+                }
+            }
+        }
+        if(count==0){
+            System.out.println("");
+        }
+    }
 }
 public class FindAllPairs {
     
