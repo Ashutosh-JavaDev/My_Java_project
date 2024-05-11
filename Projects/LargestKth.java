ca8f8ffc87fb74e92937a04702ffba41f98e0cdf
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-11 14:30:18 +0530

    Find Kth Position

--- Projects/LargestKth.java
+++ Projects/LargestKth.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 class findLargest{
     public static int kValue(int arr[],int size){
         int pos=0;
-        
+        int max=arr[0];
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                
+            }
+        }
         return pos;
     }
 }
