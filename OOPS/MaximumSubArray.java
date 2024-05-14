b3825ae10968f9eacbd99171252dd89d615ac130
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-14 22:10:48 +0530

    Sub ArraySum

--- OOPS/MaximumSubArray.java
+++ OOPS/MaximumSubArray.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 class subArraySum {
     public static void sumSubArray(int arr[], int size) {
         int sum = 0;
-        int currentSum=0;
+        int currentSum = 0;
         int count = 0;
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
-                sum=arr[i]+arr[j];
-                if(sum>currentSum){
-                    
+                sum = arr[i] + arr[j];
+                if (sum > currentSum) {
+                    System.out.println(arr[i] + " + " + arr[j] + " = " + sum);
+                    currentSum = sum;
                 }
+                sum = 0;
             }
         }
     }
