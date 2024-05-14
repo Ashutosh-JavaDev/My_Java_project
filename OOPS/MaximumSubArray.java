9486fee3656040223a1800bcfda20370dacc436f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-14 21:55:28 +0530

    Sub ArraySum

--- OOPS/MaximumSubArray.java
+++ OOPS/MaximumSubArray.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 class subArraySum{
-    
+    public static void sumSubArray(int arr[],int size){
+        int sum=0;
+        int count=0;
+        for(int i=0;i<size;i++){
+            
+        }
+    }
 }
 public class MaximumSubArray {
     
