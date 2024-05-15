cbe648186efd472510681156aa52b9c99e1c7e45
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-15 22:28:11 +0530

    Emoji Print

---
+++ my_practice/eno.java
@@ -0,0 +1,14 @@
+class Data {
+    void emoji() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("🫥");
+        }
+    }
+}
+
+public class eno {
+    public static void main(String[] args) {
+        Data ob = new Data();
+        ob.emoji();
+    }
+}
