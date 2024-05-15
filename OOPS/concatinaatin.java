b23be940a90ead027c2e916cba0bf0c6e5c34fc4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-15 22:35:52 +0530

    Concation String

--- OOPS/concatinaatin.java
+++ OOPS/concatinaatin.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Concation {
     public static String result(String str, String st) {
         String sa = " ";
-        sa = str.concat(st);
+        sa = str.concat(" "+st);
         return sa;
     }
 }
