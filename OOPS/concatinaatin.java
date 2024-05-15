080e23ee99049905d6a311cfe2ad05d8f3e97249
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-15 22:33:42 +0530

    Emoji Print

--- OOPS/concatinaatin.java
+++ OOPS/concatinaatin.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
 class Concation {
-    public static String result(String str,String st){
-        String sa=" ";
-        sa=str.concat(st);
+    public static String result(String str, String st) {
+        String sa = " ";
+        sa = str.concat(st);
         return sa;
     }
 }
 
 public class concatinaatin {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter One String: ");
+        String str = sc.nextLine();
+        System.out.print("Enter Second String: ");
+        String newStr = sc.nextLine();
+    }
 }
