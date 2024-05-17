94ba08e068c367f2436e4c09e2a99b3cba727b1d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-17 21:55:22 +0530

    Contain String

--- OOPS/ContainsString.java
+++ OOPS/ContainsString.java
@@ -14,11 +14,11 @@ public class ContainsString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String");
-        String str=sc.nextLine();
+        String str = sc.nextLine();
         System.out.println("Enter the word want to check");
-        String st=sc.nextLine();
-        containString ob=new containString();
-        boolean res=ob.containstring(str, st);
-        System.out.println("Result: "+res);
+        String st = sc.nextLine();
+        containString ob = new containString();
+        boolean res = ob.containstring(str, st);
+        System.out.println("Result: " + res);
     }
 }
