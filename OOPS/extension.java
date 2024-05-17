9a58e1f7dbf4fb265c10c07ff2e1553e8d0eba2e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-17 22:14:50 +0530

    Concation String

---
+++ OOPS/extension.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;class exten{
+    public void entens(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str = sc.nextLine();
+        System.out.println("Enter the word want to check");
+        String st = sc.nextLine();
+        containString ob = new containString();
+        boolean res = ob.containstring(str, st);
+        System.out.println("Result: " + res);
+    }
+}
+public class extension {
+    
+}
