ba29ae1fd7ce818e9c5ed0203e80279677f1c7d3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-17 21:50:24 +0530

    Contain String

--- OOPS/ContainsString.java
+++ OOPS/ContainsString.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 class containString {
-    public static boolean containstring(String str) {
-        boolean result = true;
-
+    public static boolean containstring(String str,String st) {
+        boolean result = false;
+        if(str.contains(st)){
+            result=true;
+        }
         return result;
     }
 }
