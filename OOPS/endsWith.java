1fd689e2851bfffbf32a3f4e8c32eda53408dfa2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-18 22:02:56 +0530

    Concation String

--- OOPS/endsWith.java
+++ OOPS/endsWith.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 class EndsWith{
     public boolean check(String str, String st){
         boolean result=false;
-        // if(str.endsWith(st)){
-        //     result=true;
-        // }
+        if(str.endsWith(st)){
+            result=true;
+        }
         return result;
     }
 }
