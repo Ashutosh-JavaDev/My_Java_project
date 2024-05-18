af4d6779dcdbc5397fc1b35ef03ea5646baf2a8f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-18 21:58:28 +0530

    Check EndsWith String

--- OOPS/endsWith.java
+++ OOPS/endsWith.java
@@ -9,5 +9,12 @@ class EndsWith{
     }
 }
 public class endsWith {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the String");
+        String str=sc.nextLine();
+        System.out.println("Enter the String want to check");
+        String st=sc.nextLine();
+        
+    }
 }
