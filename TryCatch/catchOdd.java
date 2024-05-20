23fa4fffca44ea143157845dd7725a1d7ac9197e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-20 22:56:49 +0530

    Catch Odd

--- TryCatch/catchOdd.java
+++ TryCatch/catchOdd.java
@@ -3,7 +3,9 @@ class Oddcatching{
     public static void catchodding(int arr[],int size){
        for(int i=0;i<size;i++){
         try{
-
+            if(arr[i]%2==0){
+                System.out.println(arr[i]);
+            }
         }
         catch(Exception e){
             e.printStackTrace();
