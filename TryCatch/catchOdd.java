a9d64bfd26fc222533323c12fd4de48ebee635fe
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-20 22:58:53 +0530

    Catch Odd

--- TryCatch/catchOdd.java
+++ TryCatch/catchOdd.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
-class Oddcatching{
-    public static void catchodding(int arr[],int size){
-       for(int i=0;i<size;i++){
-        try{
-            if(arr[i]%2==0){
-                System.out.println(arr[i]);
+
+class Oddcatching {
+    public static void catchodding(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            try {
+                if (arr[i] % 2 == 0) {
+                    System.out.println(arr[i]);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-       }
     }
 }
+
 public class catchOdd {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
-        int size=sc.nextInt();
+        int size = sc.nextInt();
     }
 }
