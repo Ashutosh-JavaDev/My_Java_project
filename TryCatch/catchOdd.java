fe3b1b606c3d28c21be5820053299258c3a31d9e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-20 23:15:03 +0530

    Try catch

--- TryCatch/catchOdd.java
+++ TryCatch/catchOdd.java
@@ -1,23 +1,10 @@
 import java.util.Scanner;
 
 class Oddcatching {
-    public static void catchodding(int arr[], int size) {
-        for (int i = 0; i < size; i++) {
-            try {
-                if (arr[i] % 2 == 0) {
-                    System.out.println(arr[i]);
-                }
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-               even(arr, size);
-            }
-        }
-    }
-    public static void even(int arr[],int size){
-        for(int i=0;i<size;i++){
-            if(arr[i]%2!=0){
-               throw new IllegalArgumentException(arr[i]+" : is odd");
-            }
+
+    public static void even(int num, int size) throws IllegalArgumentException {
+        if (num % 2 != 0) {
+            throw new IllegalArgumentException(num + " : is ODD. ");
         }
     }
 }
@@ -27,12 +14,14 @@ public class catchOdd {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
         int size = sc.nextInt();
-        int arr[]=new int[size];
+        int arr[] = new int[size];
         System.out.println("Enter the Values in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        Oddcatching ob=new Oddcatching();
-        ob.catchodding(arr, size);
+        Oddcatching ob = new Oddcatching();
+        for (int i = 0; i < size; i++) {
+
+        }
     }
 }
