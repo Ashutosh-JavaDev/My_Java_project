695321db07f63696a28ed6cf891a517b882501be
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-21 23:10:40 +0530

    Compare String

--- string/CompareTwoString.java
+++ string/CompareTwoString.java
@@ -8,5 +8,12 @@ class comparision {
 }
 
 public class CompareTwoString {
-
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the First String: ");
+    String str1=sc.nextLine();
+    System.out.println("Enter the Second String: ");
+    String str2=sc.nextLine();
+    comparision ob=new comparision();
+}
 }
