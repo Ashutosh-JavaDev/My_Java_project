73f6aadc4aa9ec4bd9afe6a15ec085d3f0bba5e3
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-21 23:06:20 +0530

    Compare String

--- string/CompareTwoString.java
+++ string/CompareTwoString.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
-class comparision{
-    public static void TwoString(String str1, String str2){
-        
+
+class comparision {
+    public static void TwoString(String str1, String str2) {
+
     }
-}public class CompareTwoString {
-    
+}
+
+public class CompareTwoString {
+
 }
