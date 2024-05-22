535fd1736f0b4560f44d1ea9ac2d44372ab10d4b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-22 12:15:21 +0530

    Sum of Digit

--- my_practice/SumofDigit.java
+++ my_practice/SumofDigit.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 class sod{
-    public static void sum(int arr[],int size){
-        
+    public static int sum(int arr[],int size){
+        int sum=0;
+        for(int i=0;i<size;i++){
+
+        }
+        return sum;
     }
 }public enum SumofDigit {
     
