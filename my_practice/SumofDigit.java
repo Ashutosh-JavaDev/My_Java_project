4483e22d554824ce76d2f342a801c5293c9fb32c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-22 12:19:08 +0530

    Sum of Digit

--- my_practice/SumofDigit.java
+++ my_practice/SumofDigit.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
-class sod{
-    public static int sum(int arr[],int size){
-        int sum=0;
-        for(int i=0;i<size;i++){
-            sum+=arr[i];
+
+class sod {
+    public static int sum(int arr[], int size) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += arr[i];
         }
         return sum;
     }
-}public enum SumofDigit {
+}public class SumofDigit{
     public static void main(String[]args){
         Scanner sc=new  Scanner(System.in);
+        System.out.println("Enter the size of Array");
+        int size=sc.nextInt();
+        
     }
 }
