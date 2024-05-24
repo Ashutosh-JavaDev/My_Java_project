e250e1b4e9f5128ad0adf76cf03d81b47e7e5fc9
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-24 14:29:57 +0530

    Login in java

--- string/Login.java
+++ string/Login.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
-class loginPage{
 
+class loginPage {
+    String Password;
+    String Checkpassword;
+    int len;
+
+    public void check() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Password: ");
+        while (true) {
+            Password = sc.nextLine();
+            if(Password.length()>8 && Password.length()<17){
+                
+            }
+        }
+
+    }
 }
+
 public class Login {
-    
+
 }
