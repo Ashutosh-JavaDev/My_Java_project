bf2ae85c0005cb9bc5d9c76e1780c672a1c83d6c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-24 14:49:46 +0530

    Login in java

--- string/Login.java
+++ string/Login.java
@@ -5,16 +5,29 @@ class loginPage {
     String Checkpassword;
     int len;
 
-    public void check() {
+    public void checkLength() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Password: ");
         while (true) {
             Password = sc.nextLine();
-            if(Password.length()>8 && Password.length()<17){
-                
+            if (Password.length() > 8 && Password.length() < 17) {
+
             }
         }
 
+    }
+
+    public boolean checkUpperLowerCase(String str) {
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            } else if (Character.isLowerCase(c)) {
+                hasLower = true;
+            }
+        }
+        return false;
     }
 }
 
