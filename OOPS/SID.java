8a199ad1f0fab1b847c455c2245fd42527f88872
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-27 20:42:09 +0530

    Search in Jswing

---
+++ OOPS/SID.java
@@ -0,0 +1,12 @@
+//WAP to Search Insert and Delete in java.
+import java.util.Scanner;
+class Search{
+    public int position(int arr[],int size){
+        int pos=0;
+        
+        return pos;
+    }
+}
+public class SID {
+    
+}
