7e406da3b5d3e86e933a1324fd38010f383a99c7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-05-27 20:49:21 +0530

    Search in Jswing

--- OOPS/SID.java
+++ OOPS/SID.java
@@ -1,20 +1,23 @@
+
 //WAP to Search Insert and Delete in java.
 import java.util.Scanner;
-class Search{
-    public int position(int arr[],int size,int target){
-        Scanner sc=new Scanner(System.in);
+
+class Search {
+    public int position(int arr[], int size, int target) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value you wants to search");
-        target=sc.nextInt();
-        int pos=0;
-        for(int i=0;i<size;i++){
-            if(arr[i]==target){
-                pos=i;
+        target = sc.nextInt();
+        int pos = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == target) {
+                pos = i;
             }
         }
         return pos;
 
     }
 }
+
 public class SID {
-    
+
 }
