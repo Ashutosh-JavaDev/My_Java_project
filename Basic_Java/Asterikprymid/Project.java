77ea8b07b6bb7a6d419b8993d55aea0714702802
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-11 19:54:38 +0530

    Java program

--- Basic_Java/Asterikprymid/Project.java
+++ Basic_Java/Asterikprymid/Project.java
@@ -3,6 +3,10 @@ import  java.util.Scanner;
 public class Project {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        
+        System.out.println("Enter the Number");
+        int number=sc.nextInt();
+        if(number%2==0){
+            System.out.println("Number is Even");
+        }
     }
 }
