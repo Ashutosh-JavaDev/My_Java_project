31b4a9723adf8516100cd40b5a0b6527d77f6746
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-11 19:58:05 +0530

    Java Program

--- Basic_Java/Asterikprymid/Project.java
+++ Basic_Java/Asterikprymid/Project.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Project {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number");
+        System.out.println("Enter the Value till the Number wants to print");
         int number = sc.nextInt();
-        if (number % 2 == 0) {
-            System.out.println("Number is Even");
-        } else {
-            System.out.println("Number is Odd");
-        }
+        int sum=0;
+       for(int i=1;i<=number;i++){
+        sum+=i;
+       }
     }
 }
