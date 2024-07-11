14703b3fa0e17728de45f251645ccada1dd40a54
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-11 20:08:53 +0530

    Java program

--- Basic_Java/Asterikprymid/Project.java
+++ Basic_Java/Asterikprymid/Project.java
@@ -2,21 +2,14 @@ package Basic_Java.Asterikprymid;
 
 import java.util.Scanner;
 
-class Animal {
+class GetterSetter {
     String name;
-    String Gender;
-    int age;
 
-    public void values() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Name of the Animal");
-        name = sc.nextLine();
-        System.out.println("Enter the Gender");
-        Gender = sc.nextLine();
-        System.out.println("Enter Age");
-        age = sc.nextInt();
-        System.out.println("Name: " + name + "Gender " + Gender + " Age " + age);
-
+    public String getname() {
+       return name;
+    }
+    public void setname(String name){
+        this.name=name;
     }
 }
 
