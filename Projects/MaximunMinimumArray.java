6ecd83f82f2a504103505dcb72a38b5d8176be0f
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-25 21:53:59 +0530

    MimimumMaximimArray

--- Projects/MaximunMinimumArray.java
+++ Projects/MaximunMinimumArray.java
@@ -24,6 +24,7 @@ public class MaximunMinimumArray {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
         int size=sc.nextInt();
+        int arr[]=new int[size];
         System.out.println("Enter the Value in the Array");
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
