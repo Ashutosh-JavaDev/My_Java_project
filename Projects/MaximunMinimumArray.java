0081b080488fb97b5dacb339176e58c816f18720
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-25 21:54:06 +0530

    MimimumMaximimArray

--- Projects/MaximunMinimumArray.java
+++ Projects/MaximunMinimumArray.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-class mimMax{
-    int size,max=0,min=0;
-    public int valuereturn(int arr[]){
-        for(int i=0;i<size;i++){
-            for(int j=i+1;j<size;j++){
-                if(arr[i]>arr[j]){
-                    max=arr[i];
+class mimMax {
+    int size, max = 0, min = 0;
+
+    public int valuereturn(int arr[]) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    max = arr[i];
                     return max;
-                }
-                else if(arr[i]<arr[j]){
-                    min=arr[i];
+                } else if (arr[i] < arr[j]) {
+                    min = arr[i];
                     return min;
                 }
             }
@@ -19,15 +19,16 @@ class mimMax{
 
     }
 }
-public class MaximunMinimumArray {   
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
+
+public class MaximunMinimumArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the Value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
     }
 }
