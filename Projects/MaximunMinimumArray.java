9bd18c30fd79137d74465d2e69d5cc69d3fd498c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-25 21:46:08 +0530

    MimimumMaximimArray

--- Projects/MaximunMinimumArray.java
+++ Projects/MaximunMinimumArray.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 class mimMax{
-    int size;
+    int size,max=0,min=0;
+    public void valuereturn(int arr[]){
+        for(int i=0;i<size;i++){
+            
+        }
+    }
 }
 public class MaximunMinimumArray {   
 }
