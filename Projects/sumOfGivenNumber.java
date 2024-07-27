5c6b460b0e1cd69ac2dcddb2280c43bf7465748e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-27 22:03:13 +0530

    Afficiation

--- Projects/sumOfGivenNumber.java
+++ Projects/sumOfGivenNumber.java
@@ -6,5 +6,8 @@ public class sumOfGivenNumber {
         int size=sc.nextInt();
         int arr[]=new int[size];
         System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
