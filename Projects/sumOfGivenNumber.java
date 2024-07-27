a06aad942c4d0791a52e9d0cee822fc6d3c1797d
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-27 22:05:49 +0530

    Afficiation

--- Projects/sumOfGivenNumber.java
+++ Projects/sumOfGivenNumber.java
@@ -12,7 +12,7 @@ public class sumOfGivenNumber {
         }
         int sum=0;
         for(int i=0;i<size;i++){
-            sum+=i;
+            sum+=arr[i];
         }
         System.out.println("Sum of "+size+" Number is :"+sum);
     }
