5ba04f29949b50bebed46ec6bd045646348ff9d8
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-27 22:03:21 +0530

    Afficiation

--- Projects/sumOfGivenNumber.java
+++ Projects/sumOfGivenNumber.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
+
 public class sumOfGivenNumber {
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
     }
 }
