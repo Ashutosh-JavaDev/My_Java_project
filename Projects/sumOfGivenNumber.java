9eaf42fae847834e6ab2e4a5044a87a66973c96a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-27 22:00:55 +0530

    Afficiation

--- Projects/sumOfGivenNumber.java
+++ Projects/sumOfGivenNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class sumOfGivenNumber {
     public static void main(String[]args){
-        
+        System.out.println("Enter the size of the Array");
     }
 }
