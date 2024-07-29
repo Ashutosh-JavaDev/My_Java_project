e68f539c12cda8c2a923b4c472a9130aa016ce1b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-29 23:41:26 +0530

    Prime

--- Projects/Prime.java
+++ Projects/Prime.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
+class PrimeNumber{
+    
+}
+
 public class Prime {
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
+
     }
 }
