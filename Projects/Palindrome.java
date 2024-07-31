713a8e853dc409de8f217e2f913ef56631d23c3e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-07-31 22:03:21 +0530

    Palindrome

--- Projects/Palindrome.java
+++ Projects/Palindrome.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
-
+class palindrome{
+    int number;
+    public void num(){
+        
+    }
+}
 public class Palindrome {
+    public static void  main(String[]args){
 
+    }
 }
