895fc3bd844b611cb24f1d41a1105a10b39064ea
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-06 21:12:34 +0530

    Duplicates

--- Projects/FindDuplicates.java
+++ Projects/FindDuplicates.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
 public class FindDuplicates {
+    int arr[],size;
     FindDuplicates() {
+        Duplicates();
+    }
+    public void Duplicates(){
 
     }
     public static void main(String[]args){
