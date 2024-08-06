47d99b8b8bb644ac55346b8997f2b54432387950
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-06 21:20:20 +0530

    Duplicates

--- Projects/FindDuplicates.java
+++ Projects/FindDuplicates.java
@@ -16,9 +16,12 @@ public class FindDuplicates {
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
+        int count=0;
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
-
+                if(arr[i]==arr[j]){
+                    count++;
+                }
             }
         }
     }
