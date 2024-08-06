a33697c50f7e0b50fb05a1a16bb42ee9facf08a0
Ashutosh Ayush <jbnashu297@gmail.com>
2024-08-06 21:16:11 +0530

    Duplicates

--- Projects/FindDuplicates.java
+++ Projects/FindDuplicates.java
@@ -13,8 +13,8 @@ public class FindDuplicates {
         size = sc.nextInt();
         arr = new int[size];
         System.out.println("Enter the Value in Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
     }
 
